1b9cf11c0e36047b8a5347daa5fb066260bfc5c7
alexandre.lemonnier <alexandre.lemonnier@epita.fr>
2022-03-26 18:03:25 +0100

    [add/refacto] dash integration

--- ALVS_sujet/environment.py
+++ ALVS_sujet/environment.py
@@ -8,19 +8,16 @@ import numpy as np
 import plotly.graph_objs as go
 import plotly.express as px
 
-class WorldPopulationStats():
+class EuropeanEnvironmentStudies():
     START = 'Start'
     STOP  = 'Stop'
 
     def __init__(self, application = None):
-        self.df = pd.read_pickle('data/subWDIdata.pkl')
-        self.continent_colors = {'Asia':'gold', 'Europe':'green', 'Africa':'brown', 'Oceania':'red', 
-                                 'Americas':'navy'}
-        self.french = {'Asia':'Asie', 'Europe':'Europe', 'Africa':'Afrique', 'Oceania':'Océanie', 'Americas':'Amériques'}
-        self.years = sorted(set(self.df.index.values))
-
+        self.df = pd.read_csv('data/europeanEnvTaxesPIB.csv')
+        self.years = sorted(set(self.df.Time.values))
+        self.pays = list(self.df.Pays.unique())
         self.main_layout = html.Div(children=[
-            html.H3(children='Évolution du taux de natalité vs le niveau moyen de revenu par pays'),
+            html.H3(children='Évolution des émissions de gaz à effet de serre selon les recettes fiscales environnementales'),
 
             html.Div('Déplacez la souris sur une bulle pour avoir les graphiques du pays en bas.'), 
 
@@ -28,11 +25,11 @@ class WorldPopulationStats():
                     html.Div([ dcc.Graph(id='wps-main-graph'), ], style={'width':'90%', }),
 
                     html.Div([
-                        html.Div('Continents'),
+                        html.Div('Pays'),
                         dcc.Checklist(
-                            id='wps-crossfilter-which-continent',
-                            options=[{'label': self.french[i], 'value': i} for i in sorted(self.continent_colors.keys())],
-                            value=sorted(self.continent_colors.keys()),
+                            id='wps-crossfilter-which-pays',
+                            options=[{'label': self.pays[i], 'value': i} for i in range(len(self.pays))],
+                            value=sorted(self.pays),
                             labelStyle={'display':'block'},
                         ),
                         html.Br(),
@@ -43,6 +40,7 @@ class WorldPopulationStats():
                             value='Log',
                             labelStyle={'display':'block'},
                         ),
+                        
                         html.Br(),
                         html.Br(),
                         html.Br(),
@@ -83,26 +81,16 @@ class WorldPopulationStats():
                 }),
 
             html.Br(),
-            html.Div(id='wps-div-country'),
-
-            html.Div([
-                dcc.Graph(id='wps-income-time-series', 
-                          style={'width':'33%', 'display':'inline-block'}),
-                dcc.Graph(id='wps-fertility-time-series',
-                          style={'width':'33%', 'display':'inline-block', 'padding-left': '0.5%'}),
-                dcc.Graph(id='wps-pop-time-series',
-                          style={'width':'33%', 'display':'inline-block', 'padding-left': '0.5%'}),
-            ], style={ 'display':'flex', 
-                       'borderTop': 'thin lightgrey solid',
-                       'borderBottom': 'thin lightgrey solid',
-                       'justifyContent':'center', }),
-
+            
+            
+            
+            
+            
         ], style={
                 #'backgroundColor': 'rgb(240, 240, 240)',
                  'padding': '10px 50px 10px 50px',
                  }
         )
-        
         if application:
             self.app = application
             # application should have its own layout and use self.main_layout as a page or in a component
@@ -114,12 +102,12 @@ class WorldPopulationStats():
         # (somhow it is more clear to have here all interaction between functions and components)
         self.app.callback(
             dash.dependencies.Output('wps-main-graph', 'figure'),
-            [ dash.dependencies.Input('wps-crossfilter-which-continent', 'value'),
+            [ dash.dependencies.Input('wps-crossfilter-which-pays', 'value'),
               dash.dependencies.Input('wps-crossfilter-xaxis-type', 'value'),
               dash.dependencies.Input('wps-crossfilter-year-slider', 'value')])(self.update_graph)
-        self.app.callback(
-            dash.dependencies.Output('wps-div-country', 'children'),
-            dash.dependencies.Input('wps-main-graph', 'hoverData'))(self.country_chosen)
+        #self.app.callback(
+        #    dash.dependencies.Output('wps-div-country', 'children'),
+        #    dash.dependencies.Input('wps-main-graph', 'hoverData'))(self.country_chosen)
         self.app.callback(
             dash.dependencies.Output('wps-button-start-stop', 'children'),
             dash.dependencies.Input('wps-button-start-stop', 'n_clicks'),
@@ -134,6 +122,7 @@ class WorldPopulationStats():
             dash.dependencies.Input('wps-auto-stepper', 'n_intervals'),
             [dash.dependencies.State('wps-crossfilter-year-slider', 'value'),
              dash.dependencies.State('wps-button-start-stop', 'children')])(self.on_interval)
+        """
         self.app.callback(
             dash.dependencies.Output('wps-income-time-series', 'figure'),
             [dash.dependencies.Input('wps-main-graph', 'hoverData'),
@@ -146,23 +135,23 @@ class WorldPopulationStats():
             dash.dependencies.Output('wps-pop-time-series', 'figure'),
             [dash.dependencies.Input('wps-main-graph', 'hoverData'),
              dash.dependencies.Input('wps-crossfilter-xaxis-type', 'value')])(self.update_pop_timeseries)
+        """
 
-
-    def update_graph(self, regions, xaxis_type, year):
-        dfg = self.df.loc[year]
-        dfg = dfg[dfg['region'].isin(regions)]
-        fig = px.scatter(dfg, x = "incomes", y = "fertility", 
+    def update_graph(self, pays, xaxis_type, year):
+        dfg = self.df[self.df.Time == year]
+        dfg = dfg[dfg['Pays'].isin(pays)]
+        fig = px.scatter(dfg, x = "PIB", y = "TAXES", 
                          #title = f"{year}", cliponaxis=False,
-                         size = "population", size_max=60, 
-                         color = "region", color_discrete_map = self.continent_colors,
-                         hover_name="Country Name", log_x=True)
+                         size = "T_HAB", size_max=40, 
+                         color = "Pays", 
+                         hover_name="Pays")
         fig.update_layout(
-                 xaxis = dict(title='Revenus net par personnes (en $ US de 2020)',
+                 xaxis = dict(title='Pourcentage du PIB utilisé pour l\'environnement',
                               type= 'linear' if xaxis_type == 'Linéaire' else 'log',
-                              range=(0,100000) if xaxis_type == 'Linéaire' 
+                              range=(0,10) if xaxis_type == 'Linéaire' 
                                               else (np.log10(50), np.log10(100000)) 
                              ),
-                 yaxis = dict(title="Nombre d'enfants par femme", range=(0,9)),
+                 yaxis = dict(title="Pourcentage des taxes pour l'environnement", range=(0,20)),
                  margin={'l': 40, 'b': 30, 't': 10, 'r': 0},
                  hovermode='closest',
                  showlegend=False,
@@ -173,7 +162,7 @@ class WorldPopulationStats():
         return {
             'data': [go.Scatter(
                 x = self.years,
-                y = self.df[self.df["Country Name"] == country][what],
+                y = self.df[self.df["Pays"] == country][what],
                 mode = 'lines+markers',
             )],
             'layout': {
@@ -188,12 +177,13 @@ class WorldPopulationStats():
 
     def get_country(self, hoverData):
         if hoverData == None:  # init value
-            return self.df['Country Name'].iloc[np.random.randint(len(self.df))]
+            return self.df['Pays'].iloc[np.random.randint(len(self.df))]
         return hoverData['points'][0]['hovertext']
 
     def country_chosen(self, hoverData):
         return self.get_country(hoverData)
 
+    """
     # graph incomes vs years
     def update_income_timeseries(self, hoverData, xaxis_type):
         country = self.get_country(hoverData)
@@ -209,7 +199,8 @@ class WorldPopulationStats():
         country = self.get_country(hoverData)
         return self.create_time_series(country, 'population', xaxis_type, 'Population')
 
-    # start and stop the movie
+    """
+       # start and stop the movie
     def button_on_click(self, n_clicks, text):
         if text == self.START:
             return self.STOP
@@ -239,5 +230,6 @@ class WorldPopulationStats():
 
 
 if __name__ == '__main__':
-    ws = WorldPopulationStats()
+    ws = EuropeanEnvironmentStudies()
     ws.run(port=8055)
+
