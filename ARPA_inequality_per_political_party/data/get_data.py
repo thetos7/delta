24e03dbb09892d8b642cccd55af980ae1e707604
alexandre.rulleau <rulleaualexandre@gmail.com>
2022-03-28 16:53:58 +0200

    WIP, trying to use animation on our graph

--- ARPA_inequality_per_political_party/data/get_data.py
+++ ARPA_inequality_per_political_party/data/get_data.py
@@ -7,11 +7,11 @@ def extract_data():
         'Luxembourg', 'Malte', 'Pays-Bas', 'Pologne', 'Portugal', 'Roumanie', 'Slovaquie', 'Slovénie', 'Espagne',
         'Suède', 'Royaume-Uni'
     ]
-    columns = ['eu', 'year', 'country', 'gov_right1', 'gov_cent1', 'gov_left1', 'gov_right3', 'gov_cent3', 'gov_left3']
+    columns = ['eu', 'year', 'country', 'iso', 'gov_right1', 'gov_cent1', 'gov_left1', 'gov_right3', 'gov_cent3', 'gov_left3']
 
-    df1 = pd.read_excel('./data/politique_mondiale.xlsx', usecols=columns).query('(eu == 1) & (year >= 2000)')
+    df1 = pd.read_excel('./politique_mondiale.xlsx', usecols=columns, skipfooter=1, dtype={'year': int}).query('(eu == 1) & (year >= 2000)')
     df1 = df1.replace(df1['country'].unique().tolist(), country_name_fr)
 
-    df2 = pd.read_excel('./data/inegalite_europe.xlsx', header=None, names=['country', 'def', 'pall', 'year', 'geni'], usecols=['country', 'year', 'geni'])
+    df2 = pd.read_excel('./inegalite_europe.xlsx', header=None, names=['country', 'def', 'pall', 'year', 'geni'], usecols=['country', 'year', 'geni'])
 
     return pd.merge(df1, df2)
