034149cd7aabbcf77d571a8ce8f565dfe9041915
Yuishiro <elodine.coquelet@epita.fr>
2022-04-08 01:17:18 +0200

    integration first graph

--- EC_CD_Mariages_et_Divorces_en_France/mariages_divorces.py
+++ EC_CD_Mariages_et_Divorces_en_France/mariages_divorces.py
@@ -14,33 +14,21 @@ from scipy import fft
 import pandas as pd
 import pandas_datareader as pdr
 import numpy as np
-import dbf
-from simpledbf import Dbf5
 
 class Mariage():
-    mar_14 = Dbf5('/data/data mariages 2014.dbf').to_dataframe()
-    mar_15 = Dbf5('/data/data mariages 2015.dbf').to_dataframe()
-    mar_16 = Dbf5('/data/data mariages 2016.dbf').to_dataframe()
-    mar_17 = Dbf5('/data/data mariages 2017.dbf').to_dataframe()
-    mar_18 = Dbf5('/data/data mariages 2018.dbf').to_dataframe()
-    mar_19 = Dbf5('/data/data mariages 2019.dbf').to_dataframe()
-    mar_20 = Dbf5('/data/data mariages 2020.dbf').to_dataframe()
-
-    def filter_df(df):
-        df.drop(columns = ['ANAIS1', 'DEPNAIS1', 'INDNAT1', 'ETAMAT1', 'ANAIS2', 'DEPNAIS2', 'INDNAT2','ETAMAT2',
-            'JSEMAINE', 'DEPDOM', 'TUDOM', 'TUCOM', 'NBENFCOM'], axis=1, inplace=True)    
-    
-    filter_df(mar_14)
-    filter_df(mar_15)
-    filter_df(mar_16)
-    filter_df(mar_17)
-    filter_df(mar_18)
-    filter_df(mar_19)
-    filter_df(mar_20)
     
     def __init__(self, application = None):
-        df = pd.concat([mar_14, mar_15, mar_16, mar_17, mar_18, mar_19, mar_20])
+        def filter_df(df):
+            df.drop(columns = ['ANAIS1', 'DEPNAIS1', 'INDNAT1', 'ETAMAT1', 'ANAIS2', 'DEPNAIS2', 'INDNAT2','ETAMAT2',
+                    'JSEMAINE', 'DEPDOM', 'TUDOM', 'TUCOM', 'NBENFCOM'], axis=1, inplace=True)
 
+        mar_14 = pd.read_csv("EC_CD_Mariages_et_Divorces_en_France/data/data_mariages_2014.csv", sep=',', low_memory=False)
+        mar_15 = pd.read_csv("EC_CD_Mariages_et_Divorces_en_France/data/data_mariages_2015.csv", sep=',', low_memory=False)
+        filter_df(mar_14)
+        filter_df(mar_15)
+        
+        df = pd.concat([mar_14, mar_15])
+       
         HH = df[(df['SEXE1'] == 'M') & (df['SEXE2'] == 'M')]
         graph = pd.DataFrame(HH.groupby('AMAR').size(), columns = ['HH'])
 
@@ -52,13 +40,18 @@ class Mariage():
 
         graph = graph.assign(TOTAL = df.groupby('AMAR').size().astype(int))
 
-        pd.options.plotting.backend = "plotly"
-        graph.plot()
-        
         self.df = graph
+
+        fig = px.line(self.df)
+
         self.main_layout = html.Div(children=[
             html.H3(children='Title'),
-            html.Div([ dcc.Graph(id='mpj-main-graph'), ], style={'width':'100%', }),
+            html.Div([
+                dcc.Graph(
+                    id='mdf-main-graph',
+                    figure=fig
+                    )
+                ])
             ])
 
         if application:
@@ -68,14 +61,34 @@ class Mariage():
             self.app = dash.Dash(__name__)
             self.app.layout = self.main_layout
 
-        self.app.callback(
-                    dash.dependencies.Output('mpj-main-graph', 'figure'),
-                    dash.dependencies.Input('mpj-mean', 'value'))(self.update_graph)
+        @self.app.callback(
+            dash.dependencies.Output("mdf-main-graph", "figure"),
+                [dash.dependencies.Input("mdf-main-type", "value")])
+        def update_line_chart(self, tmp):
+            fig = px.line(self.df)
+            return fig
 
-    def update_graph(self):
-        return self.df
 
-        
 if __name__ == '__main__':
-    mpj = Deces()
-    mpj.app.run_server(debug=True, port=8051)
+    mdf = Mariage()
+    mdf.app.run_server(debug=True, port=8051)
+    
+    
+    #mar_15 = Dbf5('data/data mariages 2015.dbf').to_dataframe()
+    #mar_16 = Dbf5('data/data mariages 2016.dbf').to_dataframe()
+    #mar_17 = Dbf5('data/data mariages 2017.dbf').to_dataframe()
+    #mar_18 = Dbf5('data/data mariages 2018.dbf').to_dataframe()
+    #mar_19 = Dbf5('data/data mariages 2019.dbf').to_dataframe()
+    #mar_20 = Dbf5('data/data mariages 2020.dbf').to_dataframe()
+
+    
+    #filter_df(mar_14)
+    #filter_df(mar_15)
+    #filter_df(mar_16)
+    #filter_df(mar_17)
+    #filter_df(mar_18)
+    #filter_df(mar_19)
+    #filter_df(mar_20)
+
+        #pd.options.plotting.backend = "plotly"
+        #graph.plot()
