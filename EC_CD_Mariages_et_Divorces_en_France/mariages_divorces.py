742ca5b0d97160652f888b94cabe9c4d7623f9be
Yuishiro <elodine.coquelet@epita.fr>
2022-04-14 12:16:27 +0200

    add change dep on click on map

--- EC_CD_Mariages_et_Divorces_en_France/mariages_divorces.py
+++ EC_CD_Mariages_et_Divorces_en_France/mariages_divorces.py
@@ -41,6 +41,7 @@ class Mariage():
         filter_df(mar_20)
         
         self.files = [mar_14, mar_15, mar_16, mar_17, mar_18, mar_19, mar_20]
+        self.dep = '01'
         
         df = pd.concat([mar_14, mar_15, mar_16, mar_17, mar_18, mar_19, mar_20])
        
@@ -73,32 +74,32 @@ class Mariage():
         mar = mar.loc[mar['DEPMAR'] == '01']
         mar = mar.drop('DEPMAR', axis=1)
 
-
+        self.year = 2014
         self.df = graph
         self.map_f = map_f
         self.departements = json.load(open('EC_CD_Mariages_et_Divorces_en_France/data/departements-version-simplifiee.geojson'))
         
-        fig = px.line(self.df)
-        fig_map = px.choropleth_mapbox(map_f, geojson=self.departements, locations= map_f.index, featureidkey = 'properties.code', 
+        self.fig = px.line(self.df)
+        self.fig_map = px.choropleth_mapbox(map_f, geojson=self.departements, locations= map_f.index, featureidkey = 'properties.code', 
                            color=2014, color_continuous_scale="Viridis",
                            mapbox_style="carto-positron",
                            zoom=4.6, center = {"lat": 47, "lon": 2},
                            opacity=0.5,
                            labels={'2014':'Nombre de Mariages'}
                           )
-        fig_map.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-        fig_histo = px.bar(mar, x="MMAR", y="Mariage", title="Bla")
+        self.fig_map.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+        self.fig_histo = px.bar(mar, x="MMAR", y="Mariage", title="Bla")
                
         self.main_layout = html.Div(children=[
             html.H3(children='Mariages en France'),
             html.Div([
                 dcc.Graph(
                     id='mdf-map-graph',
-                    figure=fig_map,
+                    figure=self.fig_map,
                     style={'width':'50%', }),
                 dcc.Graph(
                     id='mdf-main-graph',
-                    figure=fig,
+                    figure=self.fig,
                     style={'width':'50%', 'display':'inline-block'}),
                 ], style={ 'display':'flex', 
                        'borderTop': 'thin lightgrey solid',
@@ -107,7 +108,7 @@ class Mariage():
             html.Div([
                 dcc.Graph(
                     id='mdf-histo-graph',
-                    figure=fig_histo
+                    figure=self.fig_histo
                     , style={'width':'100%', }),
                 dcc.Slider(id='mdf-crossfilter-year-slider',
                     min=2014,
@@ -129,47 +130,61 @@ class Mariage():
         self.app.callback(
             dash.dependencies.Output('mdf-map-graph', 'figure'),
             dash.dependencies.Output('mdf-histo-graph', 'figure'),
-            dash.dependencies.Input('mdf-crossfilter-year-slider', 'value'))(self.update_graph)
+            dash.dependencies.Input('mdf-crossfilter-year-slider', 'value'),
+            dash.dependencies.Input('mdf-map-graph', 'clickData'))(self.update_graph)
 
 
-    def get_file(self, year):
-           return self.files[year - 2014].sort_values(by=['MMAR'])
+    def get_file(self):
+           return self.files[self.year - 2014].sort_values(by=['MMAR'])
         
-    def update_graph(self, year):
-        fig_map = px.choropleth_mapbox(self.map_f, geojson=self.departements, locations= self.map_f.index, featureidkey = 'properties.code', 
+    def update_map(self, year):
+        self.fig_map = px.choropleth_mapbox(self.map_f, geojson=self.departements,
+                        locations= self.map_f.index, featureidkey = 'properties.code', 
                         color=year, color_continuous_scale="Viridis",
                         mapbox_style="carto-positron",
                         zoom=4.6, center = {"lat": 47, "lon": 2},
                         opacity=0.5,
                         labels={year:'Nombre de Mariages'}
                         )
-        
-        mar = self.get_file(year)
+        self.fig_map.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    
+    def update_histo(self):
+        mar = self.get_file()
         mar['Mariage'] = mar['SEXE1'] + mar['SEXE2']
         mar = mar.drop(['AMAR', 'SEXE1', 'SEXE2'], axis=1)
         mar['Mariage'] =  mar['Mariage'].apply(self.str_update)
         mar['MMAR'] =  mar['MMAR'].apply(self.update_month)
-        mar = mar.loc[mar['DEPMAR'] == '01']
+        mar = mar.loc[mar['DEPMAR'] == self.dep]
         mar = mar.drop('DEPMAR', axis=1)
         
-        fig_histo = px.bar(mar, x="MMAR", y="Mariage", title="Bla")
+        self.fig_histo = px.bar(mar, x="MMAR", y="Mariage", title="Mariage et divorce dans le %s en %s" % (self.dep, self.year))
         
-        return fig_map, fig_histo
+    def update_graph(self, year, clickData):
+        ctx = dash.callback_context
+        if not ctx.triggered:
+            button_id = 'No clicks yet'
+        else:
+            button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+            
+        if button_id == 'mdf-crossfilter-year-slider':
+            self.update_map(year)
+            self.year = year
+        if button_id == 'mdf-map-graph':
+            self.dep = "%02d" % (int(clickData['points'][0]['location']))
+        if button_id != 'No clicks yeat':
+            self.update_histo()
+        
+        return self.fig_map, self.fig_histo
     
     def str_update(self, s):
         if s == 'FM':
             return 'MF'
         return s
-
         
     def update_month(self, s):
         if s not in self.L:
                 return self.L[int(s) - 1]
         return s
-    
-    def update_output(self, value):
-        return 'You have a hope : "{}"'.format(value)
-
 
 if __name__ == '__main__':
     mdf = Mariage()
