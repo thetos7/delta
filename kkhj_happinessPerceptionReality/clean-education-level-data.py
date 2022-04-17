f9d2f332c9d1945402e1b72304e4cb027417082c
karenkaspar <karenkaspar@outlook.com>
2022-04-17 18:29:06 +0200

    [CLEAN DATA] added a country column to the educatino level dataset that includes the full name of the country

--- kkhj_happinessPerceptionReality/clean-education-level-data.py
+++ kkhj_happinessPerceptionReality/clean-education-level-data.py
@@ -1,12 +1,14 @@
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import pycountry as pyc
 
 def get_means():
     df = pd.read_csv("data/educationLevel.csv")
-    mask = (df['TIME'] >= 2012) & (df['TIME'] <= 2021)
+    df.rename(columns={'TIME': 'Year'}, inplace=True)
+    mask = (df['Year'] >= 2012) & (df['Year'] <= 2021)
     df = df[mask]
-    df = (df.groupby(['LOCATION', 'TIME'], as_index=False))["Value"].mean()
+    df = (df.groupby(['LOCATION', 'Year'], as_index=False))["Value"].mean()
+    df['LOCATION'] = df['LOCATION'].apply(lambda x: pyc.countries.get(alpha_3=x).name if pyc.countries.get(alpha_3=x) != None else None)
+    df.rename(columns={'LOCATION': 'Country'}, inplace=True)
     return df
 
 if __name__ == "__main__":
