844469a139b285e0241f1c77fc7e3e4034b21760
Arthur Le Bourg <lebourgarthur@gmail.com>
2022-04-21 01:54:27 +0200

    added first plot function

--- MC_AB_consommationEtProductionEnergétique/data/get_data.py
+++ MC_AB_consommationEtProductionEnergétique/data/get_data.py
@@ -28,12 +28,6 @@ def plot_evolution(data):
     data.plot(x='TIME_PERIOD', y='OBS_VALUE', kind='line')
     plt.show()
 
-    #data = {'Year': [1920,1930,1940,1950,1960,1970,1980,1990,2000,2010],
-    #    'Unemployment_Rate': [9.8,12,8,7.2,6.9,7,6.5,6.2,5.5,6.3]
-    #   }
-    #df = pd.DataFrame(data,columns=['Year','Unemployment_Rate'])
-    #df.plot(x ='Year', y='Unemployment_Rate', kind = 'line')
-    #plt.show()
 
 def clear_unique_values(data):
     nunique = data.nunique()
@@ -42,10 +36,11 @@ def clear_unique_values(data):
 
 if __name__ == '__main__':
     data = get_data()
+    print(list_energies(data))
     FR = get_by_country(data, "FR")
-    FR_E7000 = get_by_energy(FR, "E7000")
-    unit = FR_E7000['unit'].unique()[0]
-    print(str(sum_energies(FR_E7000)) + " " + str(unit))
-    FR_cleaned = FR_E7000#clear_unique_values(FR_E7000)
-    print(FR_cleaned[FR_cleaned.TIME_PERIOD == 2004])
-    plot_evolution(FR_cleaned.nunique())
+    FR_RA000 = get_by_energy(FR, "RA000")
+    unit = FR_RA000['unit'].unique()[0]
+    print(str(sum_energies(FR_RA000)) + " " + str(unit))
+    FR_cleaned = clear_unique_values(FR_RA000)
+    FR_cleaned.reset_index(drop=True, inplace=True)
+    plot_evolution(FR_cleaned)
