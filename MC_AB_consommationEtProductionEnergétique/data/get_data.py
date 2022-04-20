26bcd7dd8dbf87a57c8b72fa194e72166857286d
Arthur Le Bourg <lebourgarthur@gmail.com>
2022-04-21 00:50:52 +0200

    error, for the same country, same energy and same date, we have more than one value

--- MC_AB_consommationEtProductionEnergétique/data/get_data.py
+++ MC_AB_consommationEtProductionEnergétique/data/get_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 def get_data():
     usecols = ['siec', 'unit', 'geo', 'TIME_PERIOD', 'OBS_VALUE']
@@ -9,5 +9,43 @@ def get_data():
     return df
 
 
+def get_by_country(data, country):
+    return data[data.geo == country]
+
+def get_by_energy(data, energy):
+    return data[data.siec == energy]
+
+def list_energies(data):
+    return data['siec'].unique()
+
+def list_countries(data):
+    return data['geo'].unique()
+
+def sum_energies(data):
+    return data["OBS_VALUE"].sum()
+
+def plot_evolution(data):
+    data.plot(x='TIME_PERIOD', y='OBS_VALUE', kind='line')
+    plt.show()
+
+    #data = {'Year': [1920,1930,1940,1950,1960,1970,1980,1990,2000,2010],
+    #    'Unemployment_Rate': [9.8,12,8,7.2,6.9,7,6.5,6.2,5.5,6.3]
+    #   }
+    #df = pd.DataFrame(data,columns=['Year','Unemployment_Rate'])
+    #df.plot(x ='Year', y='Unemployment_Rate', kind = 'line')
+    #plt.show()
+
+def clear_unique_values(data):
+    nunique = data.nunique()
+    cols_to_drop = nunique[nunique == 1].index
+    return data.drop(cols_to_drop, axis=1)
+
 if __name__ == '__main__':
-    print(get_data())
+    data = get_data()
+    FR = get_by_country(data, "FR")
+    FR_E7000 = get_by_energy(FR, "E7000")
+    unit = FR_E7000['unit'].unique()[0]
+    print(str(sum_energies(FR_E7000)) + " " + str(unit))
+    FR_cleaned = FR_E7000#clear_unique_values(FR_E7000)
+    print(FR_cleaned[FR_cleaned.TIME_PERIOD == 2004])
+    plot_evolution(FR_cleaned.nunique())
