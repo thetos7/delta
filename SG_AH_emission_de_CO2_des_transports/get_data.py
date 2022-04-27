fb2df0ae987430b74df29432787ec661aac5b684
Unikarah <sarah.gutierez@epita.fr>
2022-04-27 17:25:41 +0200

    feat(get_data): first db cleaned with function get_transport_pollution_eu

--- SG_AH_emission_de_CO2_des_transports/get_data.py
+++ SG_AH_emission_de_CO2_des_transports/get_data.py
@@ -1,1 +1,33 @@
 # Here is the script to get all the data needed in our project
+import pandas as pd
+
+def transform_countries_names(df, col):
+    countries_name = { 'AT': 'Autriche', 'BE' : 'Belgique', 'BG' : 'Bulgarie',
+                       'CH' : 'Suisse', 'CY': 'Chypre', 'CZ': 'République Tchèque',
+                       'DE' : 'Allemagne', 'DK' : 'Danemark', 'EE' : 'Estonie',
+                       'EL' : 'Grèce', 'ES' : 'Espagne', 'FI' : 'Finlande',
+                       'FR' : 'France', 'HR' : 'Croatie', 'HU' : 'Hongrie',
+                       'IE' : 'Irlande', 'IS' : 'Islande', 'IT' : 'Italie',
+                       'LI' : 'Liechtenstein', 'LT' : 'Lituanie', 'LU' : 'Luxembourg',
+                       'LV' : 'Lettonie', 'MT' : 'Malte', 'NL' : 'Pays-Bas',
+                       'NO' : 'Norvège', 'PL' : 'Pologne', 'PT' : 'Portugal',
+                       'RO' : 'Roumanie', 'SE' : 'Suède', 'SI' : 'Slovénie',
+                       'SK' : 'Slovaquie', 'TR' : 'Turquie', 'UK' : 'Royaume-Uni'}
+    df[col].replace(countries_name, inplace = True)
+    return df
+
+def get_transport_pollution_eu():
+    df = pd.read_csv("data/emission_de_polluant_des_transports_1990-2020.csv")
+    
+    # Remove the useless columns, keeping only:
+    # The data (TIME_PERIOD), The country (geo), Pollution type (airpol), the value of the pollution (OBS_VALUE) 
+    columns_to_keep = ['airpol', 'geo', 'TIME_PERIOD', 'OBS_VALUE']
+    df = df[columns_to_keep]
+
+    # Seperate data for all EUROPE
+    df_all_eu = df.loc[df['geo'] == 'EU27_2020']
+    df = df[df['geo'] != 'EU27_2020']
+    
+    # Rename geo columns with right names
+    df = transform_countries_names(df, 'geo')
+    return (df_all_eu, df[df['airpol'] == 'NMVOC'], df[df['airpol'] == 'NOX'], df[df['airpol'] == 'PM10'])
