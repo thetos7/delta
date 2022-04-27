4d803133790432873c3cd272ae832eabc46d151d
Jiayi Hao <jiayi.hao@epita.fr>
2022-04-27 18:45:51 +0200

    in comment unfinished functions

--- kkhj_happinessPerceptionReality/happiness.py
+++ kkhj_happinessPerceptionReality/happiness.py
@@ -22,37 +22,37 @@ def intersection(list1, list2):
     list = [value for value in list1 if value in list2]
     return list
 
-def get_missing_values(dataset):
-    # for all countries in datset
-    # for all years in 2012, 2021
-    # if missing then look for x - 1 if >= 2012 and x + 1 if <= 2021
-    # while one missing then -1 or +1 (or both)
-    # if one take it, else mean of the two
-    # if all missing and all over boundaries --> RAISE ERROR (but not possible)
-    # copy row to dataset --> change value and date
-
-if __name__ == "__main__":
-    perceived_happiness_dataset = get_perceived_happiness_dataset()
-    safety_dataset = safety.safety_out_of_10()
-    education_level_dataset = education.education_out_of_10()
-    gdp_dataset = gdp.gdp_out_of_10()
-    social_security_contribution_dataset = social.security_contribution_out_of_10()
-    unemployment_dataset = unemployment.unemployment_out_of_ten()
-
-    list = get_countries_list(perceived_happiness_dataset)
-
-    perceived_happiness_countries = get_countries_list(perceived_happiness_dataset)
-    safety_countries = get_countries_list(safety_dataset)
-    education_level_countries = get_countries_list(education_level_dataset)
-    gdp_countries = get_countries_list(gdp_dataset)
-    social_security_contribution_countries = get_countries_list(social_security_contribution_dataset)
-    unemployment_countries = get_countries_list(unemployment_dataset)
-
-    all_countries = intersection(intersection(intersection(intersection(perceived_happiness_countries,
-                                                                                     safety_countries), gdp_countries),
-                                              social_security_contribution_countries), unemployment_countries)
-
-    print(len(all_countries))
+# def get_missing_values(dataset):
+#     # for all countries in datset
+#     # for all years in 2012, 2021
+#     # if missing then look for x - 1 if >= 2012 and x + 1 if <= 2021
+#     # while one missing then -1 or +1 (or both)
+#     # if one take it, else mean of the two
+#     # if all missing and all over boundaries --> RAISE ERROR (but not possible)
+#     # copy row to dataset --> change value and date
+#
+# if __name__ == "__main__":
+#     perceived_happiness_dataset = get_perceived_happiness_dataset()
+#     safety_dataset = safety.safety_out_of_10()
+#     education_level_dataset = education.education_out_of_10()
+#     gdp_dataset = gdp.gdp_out_of_10()
+#     social_security_contribution_dataset = social.security_contribution_out_of_10()
+#     unemployment_dataset = unemployment.unemployment_out_of_ten()
+#
+#     list = get_countries_list(perceived_happiness_dataset)
+#
+#     perceived_happiness_countries = get_countries_list(perceived_happiness_dataset)
+#     safety_countries = get_countries_list(safety_dataset)
+#     education_level_countries = get_countries_list(education_level_dataset)
+#     gdp_countries = get_countries_list(gdp_dataset)
+#     social_security_contribution_countries = get_countries_list(social_security_contribution_dataset)
+#     unemployment_countries = get_countries_list(unemployment_dataset)
+#
+#     all_countries = intersection(intersection(intersection(intersection(perceived_happiness_countries,
+#                                                                                      safety_countries), gdp_countries),
+#                                               social_security_contribution_countries), unemployment_countries)
+#
+#     print(len(all_countries))
 
 
 # include the mask2 when counting countries #we're going to assume it's true
