848349a83e2f6037b026be95499ac63358c23d0f
Unikarah <sarah.gutierez@epita.fr>
2022-04-27 20:03:48 +0200

    feat(get_data): renamed the columns for db 1 and 2

--- SG_AH_emission_de_CO2_des_transports/get_data.py
+++ SG_AH_emission_de_CO2_des_transports/get_data.py
@@ -23,14 +23,15 @@ def get_transport_pollution_eu():
     # The data (TIME_PERIOD), The country (geo), Pollution type (airpol), the value of the pollution (OBS_VALUE) 
     columns_to_keep = ['airpol', 'geo', 'TIME_PERIOD', 'OBS_VALUE']
     df = df[columns_to_keep]
+    df = df.rename(columns= {'airpol': 'Type de Pollution', 'geo': 'Pays', 'TIME_PERIOD': 'Année', 'OBS_VALUE' :'Taux de pollution'})
 
     # Seperate data for all EUROPE
-    df_all_eu = df.loc[df['geo'] == 'EU27_2020']
-    df = df[df['geo'] != 'EU27_2020']
+    df_all_eu = df.loc[df['Pays'] == 'EU27_2020']
+    df = df[df['Pays'] != 'EU27_2020']
     
     # Rename geo columns with right names
-    df = transform_countries_names(df, 'geo')
-    return (df_all_eu, df[df['airpol'] == 'NMVOC'], df[df['airpol'] == 'NOX'], df[df['airpol'] == 'PM10'])
+    df = transform_countries_names(df, 'Pays')
+    return (df_all_eu, df[df['Type de Pollution'] == 'NMVOC'], df[df['Type de Pollution'] == 'NOX'], df[df['Type de Pollution'] == 'PM10'])
 
 def get_mean_co2_new_vehicules():
     df = pd.read_csv("data/moyenne_emissions_CO2_vehicules_neuf_2000-2020.csv")
@@ -39,13 +40,14 @@ def get_mean_co2_new_vehicules():
     # Keep : geo, TIME_PERIOD, OBS_VALUE
     columns_to_keep = ['geo', 'TIME_PERIOD', 'OBS_VALUE']
     df = df[columns_to_keep]
+    df = df.rename(columns= {'geo': 'Pays', 'TIME_PERIOD': 'Année', 'OBS_VALUE' :'Taux de pollution'})
 
     # Seperate data for all EUROPE
-    df_all_eu = df.loc[(df['geo'] == 'EU27_2020')]
-    df = df[(df['geo'] != 'EU27_2020') & (df['geo'] != 'EU27_2007') & (df['geo'] != 'EU28')]
+    df_all_eu = df.loc[(df['Pays'] == 'EU27_2020')]
+    df = df[(df['Pays'] != 'EU27_2020') & (df['Pays'] != 'EU27_2007') & (df['Pays'] != 'EU28')]
     
     # Rename geo columns with right names
-    df = transform_countries_names(df, 'geo')
+    df = transform_countries_names(df, 'Pays')
     return (df, df_all_eu)
 
 def get_air_pollution_schools():
