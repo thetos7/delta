1aa4d05f32882d9a6584840c677ec30b54940d07
Gautier Picard <gautier.picard@epita.fr>
2022-04-28 11:35:04 +0200

    just a few fixes on covid-basics notes

--- dc_sujet/covid_basics.py
+++ dc_sujet/covid_basics.py
@@ -25,17 +25,11 @@ class CovidBasics():
             Le graphique est interactif. En passant la souris sur les courbes vous avez une infobulle. 
             En utilisant les icônes en haut à droite, on peut agrandir une zone, déplacer la courbe, réinitialiser.
 
-            Sources : https://www.data.gouv.fr/fr/datasets/fichier-des-personnes-decedees/
+            Sources : https://www.data.gouv.fr/fr/datasets/synthese-des-indicateurs-de-suivi-de-lepidemie-covid-19/
             
             Notes :
-               * La grippe de l'hiver 1989-1990 a fait 20 000 morts (4,6 millions de malades en 11 semaines). La chute de la courbe au premier janvier 1990 est quand même très surprenante.
-               * On repère bien les hivers avec grippe.
-               * L'année 1997 est étrange, peut-être un problème de recensement.
-               * La canicule d'août 2003 a fait 15 000 morts (ce qui a généré la journée de travail non payé dite journée Raffarin).
-               * Les 120 000 morts dus au Covid-19 en 2020 et 2021 sont bien visibles, d'autant qu'il n'y a pas eu de grippe durant les hivers 19-20 et 20-21.
-               * On note une progression constante du nombre de morts, avec environ 1000 morts par jour en dehors de pics durant les années 70 
-                 pour environ 1700 morts par jour après 2015. Il s'agit d'une augmentation de plus de 70 %, soit plus du double que l'augmentation de la population sur la même période. Le saut visible en 1990 peut aussi traduire un recensement plus complet après cette année.
-               * Les derniers mois doivent être pris avec précaution car tous les morts ne sont pas encore recensés.
+               * On observe qu'il n'y avait pas de recensement au début de l'épidémie.
+               * On peut notamment voir les deux pics de cas, Octobre-Novembre 2020, amenant au 2nd confinement, et Janvier 2022.
             """)
         ], style={
             'backgroundColor': 'white',
