b17d27a5ad4a73309d56cdfbdc6613ff48540fa3
Romain GREGOIRE <romain.gregoire@epita.fr>
2022-04-28 15:17:34 +0200

    everything is up to date

--- Dash_application.py
+++ Dash_application.py
@@ -4,74 +4,54 @@ import matplotlib
 import glob
 import plotly.express as px
 from os import listdir
-#First We are going to create the different groups that we are going to be using
-#throughout the code
-
-colnames = ["Sex", "Type of Cancer","Age group", "Number of cases","person-years at risk"]
-
-path_africa = "CI5_treated_data/AFRICA/"
-path_europe = "CI5_treated_data/EUROPE/"
-path_north_america = "CI5_treated_data/NORTH-AMERICA/"
-path_south_america = "CI5_treated_data/SOUTH-AMERICA/"
-path_asia = "CI5_treated_data/ASIA/"
-path_oceania = "CI5_treated_data/OCEANIA/"
-
-Africa_list = []
-Europe_list = []
-North_america_list = []
-South_america_list = []
-Asia_list = []
-Oceania_list = []
-
-#Function to create Dataframe 
-def create_df_country(df_path, df_name):
-    all_files = glob.glob(df_path+df_name+"/*.csv")
-    list_Country = [pd.read_csv(files,header=None,names=colnames) for files in all_files]
-    Country = pd.concat(list_Country)
-    modify_cancer_value(Country)
-    Country = Country[Country['Type of Cancer'].str.contains("total|Unspecified")==False]
-    return Country
-
-def create_df_continent(df_path):
-    list_Country = []
-    for f in listdir(df_path):
-        current_country = create_df_country(df_path,f)
-        list_Country.append(current_country)
-    Continent = pd.concat(list_Country)
-    return Continent
-
-def create_hist(current_df, column_x, column_y, column_color, marginal_option):
-    df = px.data.tips()
-    if marginal_option == 'violin' or marginal_option == 'rug' or marginal_option == 'box':
-        fig = px.histogram(df, x=current_df[column_x],y=current_df[column_y], color=current_df[column_color],labels={'x':column_x, 'y':column_y,'color':column_color},marginal=marginal_option,text_auto=True)
-    else:
-        fig = px.histogram(df, x=current_df[column_x],y=current_df[column_y], color=current_df[column_color],labels={'x':column_x, 'y':column_y,'color':column_color},text_auto=True)
-    fig.show()
-
-#Create dash application 
+#Create dash application
+import dash
 from dash import Dash, dcc, html, Input, Output, State
 
-app = Dash(__name__)
+class Cancer():
+    #Store dataframes:
+    def __init__(self, application = None):
+        Africa = pd.read_pickle("Africa.pkl", compression="gzip")
+        Asia = pd.read_pickle("Asia.pkl", compression="gzip")
+        Europe = pd.read_pickle("Europe.pkl", compression="gzip")
+        North_america = pd.read_pickle("North_america.pkl", compression="gzip")
+        South_america = pd.read_pickle("South_america.pkl", compression="gzip")
+        Oceania = pd.read_pickle("Oceania.pkl", compression="gzip")
+        World = pd.read_pickle("World.pkl", compression="gzip")
+        
+        #Insérer la valeur du main layout représentant la page html elle même.
+        #self.main_layout = None
+        
+        if application:
+            self.app = application
+            # application should have its own layout and use self.main_layout as a page or in a component
+        else:
+            self.app = dash.Dash(__name__)
+            #self.app.layout = self.main_layout
 
-app.layout = html.Div([
-    html.Div(dcc.Input(id='input-on-submit', type='text')),
-    html.Button('Submit', id='submit-val', n_clicks=0),
-    html.Div(id='container-button-basic',
-             children='Enter a value and press submit')
-])
+        app = Dash(__name__)
+
+        self.app.layout = html.Div([
+            html.Div(dcc.Input(id='input-on-submit', type='text')),
+            html.Button('Submit', id='submit-val', n_clicks=0),
+            html.Div(id='container-button-basic',
+                 children='Enter a value and press submit')
+        ])
 
 
-@app.callback(
-    Output('container-button-basic', 'children'),
-    Input('submit-val', 'n_clicks'),
-    State('input-on-submit', 'value')
-)
-def update_output(n_clicks, value):
-    return 'The input value was "{}" and the button has been clicked {} times'.format(
-        value,
-        n_clicks
-    )
+        self.app.callback(
+            Output('container-button-basic', 'children'),
+            Input('submit-val', 'n_clicks'),
+            State('input-on-submit', 'value')
+        )
+        
+    def update_output(n_clicks, value):
+        return 'The input value was "{}" and the button has been clicked {} times'.format(
+            value,
+            n_clicks
+        )
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    cncr = Cancer()
+    cncr.app.run_server(debug=True)
