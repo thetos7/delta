2fe00468b50e99bcdc22f5ac33767809d10561b9
Moustapha DIOP <moustapha.diop@epita.fr>
2022-05-01 11:20:02 +0200

    (refacto) class

--- MDMR_NYPDCallsMeteoNY/NYPD_dash_visualisation.py
+++ MDMR_NYPDCallsMeteoNY/NYPD_dash_visualisation.py
@@ -20,28 +20,69 @@ from MDMR_NYPDCallsMeteoNY.helpers.design import (
     color_green,
 )
 from MDMR_NYPDCallsMeteoNY.helpers.utils import load_weather_data
-class SliderDataManager:
-    def __init__(self):
-        yearsM = sorted(load_weather_data().resample("M").mean().index)
-        yearsW = sorted(load_weather_data().resample("W").mean().index)
-        yearsD = sorted(load_weather_data().resample("D").mean().index)
-        self.years = {"M": yearsM, "W": yearsW, "D": yearsD}
 
-        self.range = {}
-        self.current_freq = "M"
-        self.changed = False
 
-    def get_value(self, value, freq):
-        if self.current_freq != freq:
-            return self.years.get(freq)[0]
+class MDMR_NYPDCallsMeteoNY:
+    paraf_intro = """
+    Bienvenue,  
+    Vous pourrez trouver sur cette page des comparaisons et analyses de la corrélation entre la météo
+    et le nombre d'appels passés au centre d'appel de NYPD à New-York. Chacun des graphes suivants,
+    permet de mettre en relations différents aspects météorologiques avec le nombre d'appels.  
 
-        return self.range.get(value, self.years.get(freq)[0])
+    __Remarque:__  
+    Grace au bouton ci dessous ___qui est toujours visible sur la page___, vous pourrez changer la
+    fréquence de tout les graphs de la page.  
 
-    def get_marks(self, freq):
-        years = self.years[freq]
-        self.range = {i: years[i].strftime("%Y-%m-%d") for i in range(len(years))}
-        self.current_freq = freq
-        self.changed = True
+    Trois options s'offrent à vous:
+    - par Mois
+    - par Semaine
+    - par Jour
+    """
+
+    paraf_corr = """
+    Ce graphique représente les courbes de la température moyenne et le nombre d’appels NYPD à New-York
+    sur une période de 3 ans entre le 1er janvier 2018 et le 31 décembre 2020. On remarque une très forte
+    corrélation entre les deux courbes. En effet plus la température est élevée, plus le nombre d’appels est
+    important et inversement plus la température est basse moins le nombre d’appels est élevé.  
+
+    __Remarque :__  
+    Au début de l’année 2020, on remarque une irrégularité au niveau du nombre d’appels. Cela doit être du
+    à la crise de la Covid-19.
+    """
+
+    paraf_scatter = """
+    __À noter__: vous pouvez choisir les données en taille du graphique. Deux options vous sont possibles:
+    - Vitesse du vent  
+    - Précipitations    
+
+    Ce graphique représente le nombre d’appels NYPD en fonction de la température à New-York
+    sur une période de 3 ans entre le 1er janvier 2018 et le 31 décembre 2020. La taille des bulles correspond
+    au soit: à la vitesse du vent (par défaut) ou au nombre de millimètre de précipitation. Plus une bulle est grosse, plus il y'a eu du vent/plu.
+    On remarque que le nombre d’appels est corrélé à la température mais pas corrélé à la précipitation. 
+    De plus, il est possible de constaté que, au même titre que la précipitation la vitesse du vent, ne semble
+    pas corrélé avec le nombre d'appels.
+    En effet les différentes tailles des bulles sont assez uniformément réparties sur l’ensemble du graphique.
+    """
+
+    paraf_type = """
+    Ces graphiques représentent le type est le lieu des appels NYPD par rapport à la température à New-York
+    sur une période de 3 ans entre le 1er janvier 2018 et le 31 décembre 2020. On remarque que le type d’appel
+    change en fonction de la température mais surtout qu’il y a plus d’appels dans un lieu abrité (intérieur) que 
+    dans un lieu ouvert (extérieur) quand les températures sont basses et plus d’appels à l’extérieur qu’à l’intérieur
+    quand les températures sont élevées.
+    """
+
+    def _get_value(self, value, freq):
+        if self._current_freq != freq:
+            return self._years.get(freq)[0]
+
+        return self._range.get(value, self._years.get(freq)[0])
+
+    def _get_marks(self, freq):
+        years = self._years[freq]
+        self._range = {i: years[i].strftime("%Y-%m-%d") for i in range(len(years))}
+        self._current_freq = freq
+        self._changed = True
 
         marks = {
             len(years) - 1: years[-1].strftime("%Y-%m-%d"),
@@ -61,158 +102,7 @@ class SliderDataManager:
 
         return len(years) - 1, marks
 
-
-class MDMR_NYPDCallsMeteoNY:
-    #app = Dash(__name__)
-    
-
-    def __init__(self, application=None):
-        slider_data = SliderDataManager()
-
-        frequency = {"Mois": "M", "Semaine": "W", "Jour": "D"}
-        size_values = {"Précipitations": 0, "Vittesse du vent": 1}
-
-        if application:
-            self.app = application
-            # application should have its own layout and use self.main_layout as a page or in a component
-        else:
-            self.app = dash.Dash(__name__)
-            self.app.layout = self.main_layout
-
-        self.app.css.config.serve_locally = True
-        self.app.scripts.config.serve_locally = True
-
-        # Add css file (locally hosted)
-        self.app.css.append_css({"external_url": [
-            "MDMR_NYPDCallsMeteoNY/assets/graphs.css"
-        ]})
-
-        # Serving local static files
-        @self.app.server.route('/MDMR_NYPDCallsMeteoNY/assets/<path>')
-        def static_file(path):
-            static_folder = os.path.join(os.getcwd(), 'assets')
-            return send_from_directory(static_folder, path)
-
-        @self.app.callback(
-            Output("figure-corr", "figure"),
-            Input("frequence", "value"),
-        )
-        def figure_correlation(freq):
-            return display_correlation_plot(frequency.get(freq, "M"))
-
-        @self.app.callback(
-            Output("figure-scatter", "figure"),
-            Input("frequence", "value"),
-            Input("size_scatter", "value"),
-        )
-        def scatter_figure(freq, size_value):
-            return display_correlation_scatter(
-                frequency.get(freq, "M"), size_values.get(size_value, 1)
-            )
-
-        @self.app.callback(
-            Output("figure-types", "figure"),
-            Output("figure-types-in-out", "figure"),
-            Input("frequence", "value"),
-            Input("slider", "value"),
-        )
-        def figure_types(freq, value):
-            freq = frequency.get(freq, "M")
-            value = slider_data.get_value(value, freq)
-
-            return (types_of_calls(freq, value=value), in_out_of_calls(freq, value=value))
-
-        @self.app.callback(
-            Output("slider", "min"),
-            Output("slider", "max"),
-            Output("slider", "marks"),
-            Input("frequence", "value"),
-        )
-        def slider_years(freq):
-            return 0, *slider_data.get_marks(frequency.get(freq, "M"))
-
-        @self.app.callback(
-            Output("slider", "value"),
-            Input("slider", "value"),
-            Input("stepper", "disabled"),
-            Input("stepper", "n_intervals"),
-        )
-        def update_slider(value, disable, _):
-            if disable:
-                return value
-
-            if slider_data.changed:
-                slider_data.changed = False
-                return 0
-
-            j = len(slider_data.range)
-            return 0 if j == 0 else (value + 1) % j
-
-        @self.app.callback(Output("date-slider", "children"), Input("slider", "value"))
-        def display_value(value):
-            return f"Date : {slider_data.range.get(value)}"
-
-        @self.app.callback(
-            Output("play_pause_button", "children"),
-            Output("stepper", "disabled"),
-            Input("play_pause_button", "n_clicks"),
-            Input("play_pause_button", "children"),
-        )
-        def play_pause_button(_, children):
-            if children == "Start":
-                return "Stop", False
-
-            return "Start", True
-
-        paraf_intro = """
-        Bienvenue,  
-        Vous pourrez trouver sur cette page des comparaisons et analyses de la corrélation entre la météo
-        et le nombre d'appels passés au centre d'appel de NYPD à New-York. Chacun des graphes suivants,
-        permet de mettre en relations différents aspects météorologiques avec le nombre d'appels.  
-
-        __Remarque:__  
-        Grace au bouton ci dessous ___qui est toujours visible sur la page___, vous pourrez changer la
-        fréquence de tout les graphs de la page.  
-
-        Trois options s'offrent à vous:
-        - par Mois
-        - par Semaine
-        - par Jour
-        """
-
-        paraf_corr = """
-        Ce graphique représente les courbes de la température moyenne et le nombre d’appels NYPD à New-York
-        sur une période de 3 ans entre le 1er janvier 2018 et le 31 décembre 2020. On remarque une très forte
-        corrélation entre les deux courbes. En effet plus la température est élevée, plus le nombre d’appels est
-        important et inversement plus la température est basse moins le nombre d’appels est élevé.  
-
-        __Remarque :__  
-        Au début de l’année 2020, on remarque une irrégularité au niveau du nombre d’appels. Cela doit être du
-        à la crise de la Covid-19.
-        """
-
-        paraf_scatter = """
-        __À noter__: vous pouvez choisir les données en taille du graphique. Deux options vous sont possibles:
-        - Vitesse du vent  
-        - Précipitations    
-
-        Ce graphique représente le nombre d’appels NYPD en fonction de la température à New-York
-        sur une période de 3 ans entre le 1er janvier 2018 et le 31 décembre 2020. La taille des bulles correspond
-        au soit: à la vitesse du vent (par défaut) ou au nombre de millimètre de précipitation. Plus une bulle est grosse, plus il y'a eu du vent/plu.
-        On remarque que le nombre d’appels est corrélé à la température mais pas corrélé à la précipitation. 
-        De plus, il est possible de constaté que, au même titre que la précipitation la vitesse du vent, ne semble
-        pas corrélé avec le nombre d'appels.
-        En effet les différentes tailles des bulles sont assez uniformément réparties sur l’ensemble du graphique.
-        """
-
-        paraf_type = """
-        Ces graphiques représentent le type est le lieu des appels NYPD par rapport à la température à New-York
-        sur une période de 3 ans entre le 1er janvier 2018 et le 31 décembre 2020. On remarque que le type d’appel
-        change en fonction de la température mais surtout qu’il y a plus d’appels dans un lieu abrité (intérieur) que 
-        dans un lieu ouvert (extérieur) quand les températures sont basses et plus d’appels à l’extérieur qu’à l’intérieur
-        quand les températures sont élevées.
-        """
-
+    def _layout(self):
         self.main_layout = html.Div(
             style=app_base,
             children=[
@@ -222,7 +112,7 @@ class MDMR_NYPDCallsMeteoNY:
                 html.Div(
                     style=app_intro,
                     children=[
-                        dcc.Markdown(paraf_intro),
+                        dcc.Markdown(MDMR_NYPDCallsMeteoNY.paraf_intro),
                     ],
                 ),
                 html.Div(
@@ -247,7 +137,7 @@ class MDMR_NYPDCallsMeteoNY:
                             children="Nombre d'appels et température moyenne"
                         ),
                         html.Div([dcc.Graph(style={**graph_div, **graph}, id="figure-corr")]),
-                        dcc.Markdown(paraf_corr, style=graph_text),
+                        dcc.Markdown(MDMR_NYPDCallsMeteoNY.paraf_corr, style=graph_text),
                     ],
                 ),
                 html.Div(
@@ -272,7 +162,7 @@ class MDMR_NYPDCallsMeteoNY:
                                 ),
                             ],
                         ),
-                        dcc.Markdown(paraf_scatter, style=graph_text),
+                        dcc.Markdown(MDMR_NYPDCallsMeteoNY.paraf_scatter, style=graph_text),
                     ],
                 ),
                 html.Div(
@@ -337,7 +227,7 @@ class MDMR_NYPDCallsMeteoNY:
                                         ),
                                     ],
                                 ),
-                                dcc.Markdown(children=paraf_type, style=graph_text),
+                                dcc.Markdown(children=MDMR_NYPDCallsMeteoNY.paraf_type, style=graph_text),
                             ],
                         ),
                     ]
@@ -366,6 +256,101 @@ class MDMR_NYPDCallsMeteoNY:
                 ),
             ],
         )
+
+    def __init__(self, application=None):
+        self._layout()
+
+        if application:
+            self.app = application
+        else:
+            self.app = dash.Dash(__name__)
+            self.app.layout = self.main_layout
+
+        yearsM = sorted(load_weather_data().resample("M").mean().index)
+        yearsW = sorted(load_weather_data().resample("W").mean().index)
+        yearsD = sorted(load_weather_data().resample("D").mean().index)
+
+        self._years = {"M": yearsM, "W": yearsW, "D": yearsD}
+        self._range = {}
+        self._current_freq = "M"
+        self._changed = False
+        self._frequency = {"Mois": "M", "Semaine": "W", "Jour": "D"}
+        self._size_values = {"Précipitations": 0, "Vittesse du vent": 1}
+
+        self.app.callback(
+            Output("figure-corr", "figure"),
+            Input("frequence", "value"))(self.figure_correlation)
+
+        self.app.callback(
+            Output("figure-scatter", "figure"),
+            [Input("frequence", "value"),
+             Input("size_scatter", "value")])(self.scatter_figure)
+
+        self.app.callback(
+            [Output("figure-types", "figure"),
+             Output("figure-types-in-out", "figure")],
+            [Input("frequence", "value"),
+             Input("slider", "value")])(self.figure_types)
+
+        self.app.callback(
+            [Output("slider", "min"),
+             Output("slider", "max"),
+             Output("slider", "marks")],
+            Input("frequence", "value"))(self.slider_years)
+
+        self.app.callback(
+            Output("slider", "value"),
+            [Input("slider", "value"),
+             Input("stepper", "disabled"),
+             Input("stepper", "n_intervals")])(self.update_slider)
+
+        self.app.callback(
+            Output("date-slider", "children"),
+            Input("slider", "value"))(self.display_value)
+
+        self.app.callback(
+            [Output("play_pause_button", "children"),
+             Output("stepper", "disabled")],
+            [Input("play_pause_button", "n_clicks"),
+             Input("play_pause_button", "children")])(self.play_pause_button)
+
+
+    def figure_correlation(self, freq):
+        return display_correlation_plot(self._frequency.get(freq, "M"))
+
+    def scatter_figure(self, freq, size_value):
+        return display_correlation_scatter(
+            self._frequency.get(freq, "M"), self._size_values.get(size_value, 1)
+        )
+
+    def figure_types(self, freq, value):
+        freq = self._frequency.get(freq, "M")
+        value = self._get_value(value, freq)
+
+        return (types_of_calls(freq, value=value), in_out_of_calls(freq, value=value))
+
+    def slider_years(self, freq):
+        return 0, *self._get_marks(self._frequency.get(freq, "M"))
+
+    def update_slider(self, value, disable, _):
+        if disable:
+            return value
+
+        if self._changed:
+            self._changed = False
+            return 0
+
+        j = len(self._range)
+        return 0 if j == 0 else (value + 1) % j
+
+    def display_value(self, value):
+        return f"Date : {self._range.get(value)}"
+
+    def play_pause_button(self, _, children):
+        if children == "Start":
+            return "Stop", False
+
+        return "Start", True
 
 
 if __name__ == "__main__":
