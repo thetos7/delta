e4f7d606f7fc726714f936d68a5c673a7876d10e
Ancelin Bouchet <ancelin.bouchet@epita.fr>
2022-05-03 22:32:39 +0200

    Added basic folium integration into dash

--- pgab_accidents/accidents.py
+++ pgab_accidents/accidents.py
@@ -1,14 +1,36 @@
+from typing import Union
 import dash
 from dash import html
+import dash_core_components as dcc
+import os
 
 # TODO create layout
 class Accidents:
     def __init__(self, application = None):
+        file_dir = os.path.dirname(os.path.realpath(__file__))
+        map_html: Union[str, None] = None
+        try:
+            with open(f'{file_dir}/map.html', 'r') as map_file:
+                map_html = map_file.read()
+        except FileNotFoundError: # Allow missing map file, for now
+            pass
+
         self.main_layout = html.Div(children=[
-            html.H3(children='Répartition des accidents de la route en France métropolitaine en 2018')
+            html.H3(children='Répartition des accidents de la route en France métropolitaine en 2018'),
+            html.Iframe(
+                srcDoc=map_html,
+                style={
+                    'min-height': '80vh'
+                }
+            ) if map_html is not None else dcc.Markdown("""
+            Map HTML file is missing.  
+            It may not have generated correctly.
+            """),
         ], style={
             'backgroundColor': 'white',
             'padding': '10px 50px 10px 50px',
+            'display': 'flex',
+            'flex-direction': 'column'
         })
 
         if application:
