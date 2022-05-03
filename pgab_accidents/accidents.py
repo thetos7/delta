f384b022d1922995bdf829c586b115f42aa59675
Ancelin Bouchet <ancelin.bouchet@epita.fr>
2022-05-03 22:55:22 +0200

    Refactored dashboard

--- pgab_accidents/accidents.py
+++ pgab_accidents/accidents.py
@@ -8,9 +8,10 @@ import os
 class Accidents:
     def __init__(self, application = None):
         file_dir = os.path.dirname(os.path.realpath(__file__))
+        map_filepath = f'{file_dir}/map.html' 
         map_html: Union[str, None] = None
         try:
-            with open(f'{file_dir}/map.html', 'r') as map_file:
+            with open(map_filepath, 'r') as map_file:
                 map_html = map_file.read()
         except FileNotFoundError: # Allow missing map file, for now
             pass
@@ -24,8 +25,8 @@ class Accidents:
                     'min-width': '40vw',
                     'align-self': 'center'
                 }
-            ) if map_html is not None else dcc.Markdown("""
-            Map HTML file is missing.  
+            ) if map_html is not None else dcc.Markdown(f"""
+            Map HTML file `{map_filepath}` is missing.  
             It may not have generated correctly.
             """),
             dcc.Markdown("""
