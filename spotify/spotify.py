00ca741d34c40dfcf74006b923b7123e077bbed8
melvin.gidel <melvin.gidel@epita.fr>
2022-05-06 17:54:38 +0200

    added graph to website

--- spotify/spotify.py
+++ spotify/spotify.py
@@ -1,4 +1,6 @@
 import sys
+
+from requests import NullHandler
 import dash
 import flask
 from dash import dcc
@@ -10,15 +12,31 @@ import plotly.express as px
 import dateutil as du
 
 class Spotify():
-    def _make_dataframe(filename):
-        df = pd.read_csv(filename, sep=";", encoding="cp1252", skiprows=[0,1,3], header=None)
-        return df
+
+    def format_caracteristiques():
+        df = pd.read_csv('spotify/data/SpotifyFeatures.csv')
+        df = df.rename(columns={'artist_name':'artist', 'track_name':'title'})
+        dfmean = df.drop(columns=['artist', 'title', 'track_id', 'key', 'mode', 'time_signature'])
+        dfmean = dfmean.groupby(['genre']).mean()
+        return dfmean
 
     def __init__(self, application = None):
+        self.caracteristiques = Spotify.format_caracteristiques()
 
         self.main_layout = html.Div(children=[
             html.H3(children='Caractéristiques de la popularité d\'une musique sur Spotify'),
             html.Div([ dcc.Graph(id='spo-main-graph'), ], style={'width':'100%', }),
+            html.Div([
+                html.Div([ html.Div('Spotify'),
+                           dcc.RadioItems(
+                               id='spo-spo',
+                               options=[{'label':'Oui', 'value':0}, 
+                                        {'label':'Non','value':1}],
+                               value=1,
+                               labelStyle={'display':'block'},
+                           )
+                         ], style={'width': '9em'} )
+                    ]),
             html.Br(),
             dcc.Markdown("""
                 #### À propos
@@ -41,7 +59,16 @@ class Spotify():
             self.app = dash.Dash(__name__)
             self.app.layout = self.main_layout
 
-        
+        self.app.callback(
+                    dash.dependencies.Output('spo-main-graph', 'figure'),
+                    dash.dependencies.Input('spo-spo', 'value'))(self.update_graph)
+
+    def update_graph(self, spo):
+        df = self.caracteristiques
+        fig = px.scatter(df, x='popularity', y='danceability', color=df.index, hover_name=df.index, log_x=False,
+                         title="Popularity of genres through the dance"
+                        )
+        return fig
 
 if __name__ == '__main__':
     spo = Spotify()
