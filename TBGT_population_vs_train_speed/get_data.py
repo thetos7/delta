48b93e74e7f29f0752646887dc862e4a1db01a77
Guillaume THOMAS <guillaume1.thomas@epita.fr>
2022-05-06 15:02:49 +0200

    Add population data

--- TBGT_population_vs_train_speed/get_data.py
+++ TBGT_population_vs_train_speed/get_data.py
@@ -34,3 +34,36 @@ def get_cities() -> dict[str, Coords]:
         cities = json.load(f)
 
     return cities
+
+def _clean_pop_index(pop_df: pd.DataFrame) -> None:
+    # Remove useless columns/ rows
+    pop_df = pop_df.drop(index=[0,1,2]).drop(columns=pop_df.columns[0:3])
+    pop_df.columns = pop_df.iloc[0]
+    pop_df.drop(index=[3,4], inplace=True)
+
+    # Rename cities column
+    pop_df = pop_df.set_index("Libellé géographique")
+    pop_df.columns = [[int(s) for s in name.split() if s.isdigit()][0] for name in pop_df.columns]
+    pop_df.index = pop_df.index.str.upper()
+    pop_df.index = pop_df.index.str.replace(u"É","E")
+
+    # Compute Paris as a single city
+    paris = pop_df.loc["Paris 1er Arrondissement":"Paris 20e Arrondissement"].sum()
+    paris.name = "Paris"
+    pop_df = pop_df.append(paris.transpose())
+
+    # Sort on years
+    pop_df = pop_df.transpose()
+    pop_df = pop_df.sort_index()
+
+    # Interpolate lacking data
+    pop_df = pop_df.reindex(list(range(pop_df.index.min(),pop_df.index.max()+1)))
+    pop_df = pop_df.infer_objects()
+    pop_df = pop_df.interpolate()
+    growth = pop_df.pct_change().mul(100)
+
+def get_population() -> pd.DataFrame:
+    pop_df = pd.read_excel("TBGT_population_vs_train_speed/data/base-pop-historiques-1876-2019.xlsx")
+    _clean_pop_index(pop_df)
+
+    return pop_df
