9f335669704b825aa13792eab5d5584e8cc3f692
Guillaume THOMAS <guillaume1.thomas@epita.fr>
2022-05-06 23:34:10 +0200

    Move legend inside graph
    It is more readable this way

--- TBGT_population_vs_train_speed/TBGT_population_vs_train_speed.py
+++ TBGT_population_vs_train_speed/TBGT_population_vs_train_speed.py
@@ -73,11 +73,18 @@ class TBGT:
 
         return fig
 
+    def _set_legend(self, fig: go.Figure) -> None:
+        fig.update_layout(legend=dict(
+            yanchor="top",
+            xanchor="right",
+        ))
+
     def _get_global_line_speed_fig(self) -> go.Figure:
         fig = self.train_df.groupby("Année").mean().interpolate(method='linear').plot()
         fig.update_xaxes(title_text="Années")
         fig.update_yaxes(title_text="Minutes")
         fig.update_layout(title="Evolution du temps de trajet moyen des grandes lignes en Frances")
+        self._set_legend(fig)
 
         return fig
 
@@ -95,6 +102,7 @@ class TBGT:
         fig.update_xaxes(title_text="Années")
         fig.update_yaxes(title_text="Minutes")
         fig.update_layout(title=f"Evolution du temps de trajet sur la ligne {relation}")
+        self._set_legend(fig)
 
         return fig
 
@@ -111,6 +119,7 @@ class TBGT:
                                legendgroup = category[t.name],
                                hovertemplate = t.hovertemplate.replace(t.name, category[t.name]))
         )
+        self._set_legend(fig)
 
         return fig
 
@@ -122,6 +131,7 @@ class TBGT:
         fig.update_xaxes(title_text="Années")
         fig.update_yaxes(title_text="Croissance en %")
         fig.update_layout(title="Croissance de la population des deux villes")
+        self._set_legend(fig)
 
         return fig
 
