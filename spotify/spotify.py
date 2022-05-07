f7da81707b374b91baaac846cde44d738ff5d964
melvin.gidel <melvin.gidel@epita.fr>
2022-05-07 19:51:54 +0200

    add sub genre graph

--- spotify/spotify.py
+++ spotify/spotify.py
@@ -39,7 +39,7 @@ class Spotify():
         self.count = count
 
     def __init__(self, application = None):
-        self.french = {'popularity':'popularité', 'acousticness':'accoustique', 'danceability':'dansabilité', 'duration_ms':'temps en ms',
+        self.french = {'popularity':'popularité', 'acousticness':'acousticité', 'danceability':'dansabilité', 'duration_ms':'durée en ms',
         'energy':'énergie', 'instrumentalness':'instrumentalité', 'liveness':'vivacité', 'loudness':'intensité sonore', 'speechiness':'quantité de paroles',
         'tempo':'tempo', 'valence':'valence'}
 
@@ -52,7 +52,7 @@ class Spotify():
                 
                 html.Div([
                     html.Div([
-                        html.Div('Caractéristique axe x'),
+                        html.Div('Caractéristique axe x :'),
                         dcc.RadioItems(
                             id='x-charac',
                             options=[{'label':self.french[self.characteristics.columns[i]], 'value':i} for i in range(len(self.characteristics.columns))],
@@ -63,7 +63,7 @@ class Spotify():
                         html.Div('Moyenne par genre sur le top :'),
                         dcc.RadioItems(
                             id='topten',
-                            options=[{'label':'10 000', 'value':False},
+                            options=[{'label':'~ 10 000', 'value':False},
                                      {'label':'10', 'value':True}],
                             value=False,
                             labelStyle={'display':'block'},
@@ -74,6 +74,9 @@ class Spotify():
                     'padding-right': '30px', 
                     'display':'flex'
                 }),
+            
+            html.Div('Cliquez sur une bulle pour avoir le diagramme polaire du genre ci-dessous.'),
+            html.Br(),
             html.Div([ 
                     html.Div('Échelle'),
                     dcc.RadioItems(
@@ -85,13 +88,19 @@ class Spotify():
                     ]),
             html.Br(),
             html.Br(),
+            html.Div([
+                dcc.Graph(id='genre-charac', 
+                          style={'width':'50%'}),
+            ]),
+            html.Br(),
+            html.Br(),
             dcc.Markdown("""
                 #### Notes :
                     
                 La popularité d'un genre est un pourcentage. Celle d'une musique correspond à son nombre d'écoutes par rapport au nombre d'écoutes de la musique la plus écoutée.
                 Ainsi, la valeur utilisée correspond à la moyenne des popularités des musiques d'un genre
                 
-                Les autres caractériques du graphique sont définies à cette adresse : [Caractéristiques audio d'une musique](https://developer.spotify.com/documentation/web-api/reference/#/operations/get-audio-features)
+                Les autres caractéristiques du graphique sont définies à cette adresse : [Caractéristiques audio d'une musique](https://developer.spotify.com/documentation/web-api/reference/#/operations/get-audio-features)
                 """),
             html.Br(),
             dcc.Markdown("""
@@ -121,6 +130,9 @@ class Spotify():
                     dash.dependencies.Input('log', 'value'),
                     dash.dependencies.Input('topten', 'value')
                     )(self.update_graph)
+        self.app.callback(
+                    dash.dependencies.Output('genre-charac', 'figure'),
+                    dash.dependencies.Input('charac-main-graph', 'clickData'))(self.update_genre_graph)
 
     def update_graph(self, x_carac, log, topten):
         df,size = (self.characteristics,self.count) if not(topten) else (self.topten,[10 for _ in self.genres])
@@ -135,6 +147,53 @@ class Spotify():
                         size=size,
                         height=750,
                     )
+        return fig
+
+    def get_iris_fig(self, genre):
+        dfmean = self.musics
+        dfmean = dfmean.drop(columns=['artist', 'title', 'track_id', 'key', 'mode', 'time_signature'])
+        genre_df = dfmean.where(dfmean['genre'] == genre).dropna().groupby(['genre']).mean()
+        
+        features=[genre_df['popularity'],genre_df['acousticness'],genre_df['danceability'],genre_df['energy'],genre_df['instrumentalness'],genre_df['speechiness'],
+                genre_df['valence'],genre_df['liveness'],genre_df['loudness']]
+        ranges=[100.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -60.0]
+
+        for index in range(len(ranges)):
+            features[index] = round(features[index]/ranges[index], 3)
+
+        list = []
+
+        for i in range(len(features)):
+            list.append(features[i].values[0])
+
+        theta = ['popularity', 'acousticness', 'danceability', 'energy', 'instrumentalness', 'speechiness', 'valence', 'liveness', 'loudness']
+        fig = go.Figure(data=go.Scatterpolar(
+            r = list,
+            theta = [self.french[i] for i in theta],
+            fill = 'toself'
+            ))
+
+        fig.update_layout(
+            polar=dict(
+            radialaxis=dict(
+                visible=True,range=[0,1.0]
+            ),
+            ),
+            showlegend=False,
+            title = 'Diagramme polaire des caractéristiques du genre "' + str(genre) + '"',
+            title_x=0.5
+        )
+
+        return fig
+
+    def get_genre(self, clickData):
+        if clickData == None:  # init value
+            return self.genres[np.random.randint(len(self.genres))]
+        return clickData['points'][0]['hovertext']
+
+    def update_genre_graph(self, clickData):
+        genre = self.get_genre(clickData)
+        fig = self.get_iris_fig(genre)
         return fig
 
 if __name__ == '__main__':
