fc85abfeb4684a79f1e9ab2942111bce6dedc112
Antoine Zellmeyer <antoine.zellmeyer@epita.fr>
2022-05-07 03:07:37 +0200

    Remove RIOT API KEY

--- LeagueOfLegends/DataGathering/RiotAPIScrapper.py
+++ LeagueOfLegends/DataGathering/RiotAPIScrapper.py
@@ -5,7 +5,7 @@ import json
 
 class RiotAccess:
 
-    RIOT_API_KEY = "RGAPI-a109608f-8d8c-4742-b4c1-cae077519e09"
+    RIOT_API_KEY = ""
     EU_RIOT_MATCH_ENDPOINT = "https://europe.api.riotgames.com/lol/match/v5/matches/"
     ASIA_RIOT_MATCH_ENDPOINT="https://asia.api.riotgames.com/lol/match/v5/matches/"
     NA_RIOT_MATCH_ENDPOINT="https://americas.api.riotgames.com/lol/match/v5/matches/"
@@ -30,7 +30,15 @@ class RiotAccess:
     def __init__(self):
         self.headers = {"X-Riot-Token": self.RIOT_API_KEY}
 
+    def update_api_key(self, newkey):
+        self.RIOT_API_KEY = newkey
+        self.headers = {"X-Riot-Token": self.RIOT_API_KEY}
+
     def get_match_id_info(self, matchID):
+        """
+        :param matchID: ID of a league of legends match
+        :return: a json of what everything that happened during that game
+        """
         endpoint = self.END_POINT_MATCH[matchID.split("_")[0]]
         try:
             req = requests.get(endpoint+matchID, headers=self.headers)
@@ -39,7 +47,13 @@ class RiotAccess:
             print("[ERROR] get_match_id_info : ", str(e))
             return None
 
-    def save_data_from_source(self, start_id=0,max_id=None):
+    def save_data_from_source(self, start_id=0,max_id=None,outpath="out_data.json", filter_func=None,max_it=-1):
+        """
+        :param start_id: start reading data from this line number
+        :param max_id: stop reading data from this line number
+        :param outpath: save location of all matches into a big json file
+        :return:
+        """
         try:
             id_file = pd.read_csv(self.MATCH_ID_PATH)
         except:
@@ -47,20 +61,22 @@ class RiotAccess:
             return
 
         jsonout = {}
+        it = 0
 
         for k,ID in enumerate(id_file['MATCH_ID']):
-            if k < start_id:
+            if k < start_id or (filter_func is not None and not filter_func(ID)):
                 continue
-            elif max_id is not None and k > max_id:
+            elif (max_id is not None and k > max_id) or it > max_it:
                 break
             data = self.get_match_id_info(ID)
             if data is not None:
                 jsonout[ID] = data
 
+            it += 1
             # WAITING TIME BETWEEN REQUESTS
             time.sleep(self.REQUEST_SLEEP_TIME)
 
-        with open('out_data.json', 'w') as f:
+        with open(outpath, 'w') as f:
             json.dump(jsonout, f)
 
 
@@ -69,4 +85,5 @@ class RiotAccess:
 
 if __name__ == "__main__": #For debug purposes only
     riot_app = RiotAccess()
-    print(riot_app.get_match_id_info("RU_343155688"))
+    riot_app.save_data_from_source(filter_func=lambda id: "EUW1" in id, outpath="MatchesID/EUW1_1200_GAME_DATA.json", max_it=1200)
+    #print(riot_app.get_match_id_info("RU_343155688"))
