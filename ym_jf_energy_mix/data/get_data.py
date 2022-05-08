06a1c34134e8fdfa795585c3ba85c1110a4cff49
Nyacreeps <yorick.madelain@epita.fr>
2022-05-08 09:51:04 +0200

    updated get_data script

--- ym_jf_energy_mix/data/get_data.py
+++ ym_jf_energy_mix/data/get_data.py
@@ -1,23 +1,16 @@
 # must be run from file root (python3 get_data.py)
-# step 1 : have a kaggle account https://www.kaggle.com/
-# step 2 : install kaggle API : pip install kaggle
-# step 3 : go to account -> scroll down to API -> click Create token
-# step 4 : move file to ~/kaggle/kaggle.json
-# step 5 : run python file
 
-# if it doesn't work or you don't want to waste your time, just download the file from the website at
+# if it doesn't work just download the file from the website at
 # https://www.kaggle.com/datasets/pralabhpoudel/world-energy-consumption
 # rename it to World_Energy_Consumption.csv (note the underscores)
 # place it in data/ and ym_jf_energy_mix/data/
 
-import kaggle
-import os
+import requests
 import shutil
 
-kaggle.api.authenticate()
+URL = "https://raw.githubusercontent.com/owid/energy-data/master/owid-energy-data.csv"
 
-kaggle.api.dataset_download_files('pralabhpoudel/world-energy-consumption', path='.', unzip=True)
+a = requests.get(URL)
 
-os.rename("./World Energy Consumption.csv", "./World_Energy_Consumption.csv")
-
+open('World_Energy_Consumption.csv', 'wb').write(a.content)
 shutil.copyfile("./World_Energy_Consumption.csv", "../../data/World_Energy_Consumption.csv")
