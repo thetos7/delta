d20de22238412aa0a77bd822b26eaabfe9471d17
felix <felix.wirth@epita.fr>
2022-05-08 22:57:32 +0200

    [PYBD] Adding scatter graph

--- formations/formations.py
+++ formations/formations.py
@@ -18,29 +18,56 @@ class Formations:
         self.df = pd.read_csv(os.getcwd() + "/formations/data/formations.csv", low_memory=False)
 
         self.years = self.df["Année universitaire"].unique().tolist()
+        self.levels = ["Niveau dans le diplôme", "Grande discipline", "Secteur disciplinaire"]
         self.years.sort()
-        cursus_data = self.df.groupby(
+        self.cursus_data = self.df.groupby(
             ["Année universitaire", "Type d'établissement", "Grande discipline"]).sum()
-        cursus_data["Proportion Femmes"] = cursus_data["Dont femmes"] / cursus_data["Nombre d\'étudiants"]
-        cursus_data = cursus_data.reset_index([1, 2])
+        self.cursus_data["Proportion Femmes"] = self.cursus_data["Dont femmes"] / self.cursus_data[
+            "Nombre d\'étudiants"]
+        self.cursus_data = self.cursus_data.reset_index([1, 2])
 
         self.main_layout = html.Div([
             html.H4("Treemap de la proportion H/F par type d'établissement et par Grande discipline"),
-            dcc.Graph(id="graph"),
+            dcc.Graph(id="treemap"),
             html.P("Year:"),
             dcc.Slider(id="year", min=0, max=len(self.years) - 1, value=0, step=1,
                        marks=dict(enumerate(self.years))),
             dcc.Markdown(
                 '*Utilisez le slider pour voir la répartition H/F parmis les différentes formations au cours des années'
-                'années*')
+                'années*'),
+            dcc.Dropdown(self.levels, self.levels[0], id='level_selection'),
+            dcc.Graph(id="scatter")
         ]
         )
 
         @application.callback(
-            Output("graph", "figure"),
+            Output('scatter', 'figure'),
+            Input('level_selection', 'value')
+        )
+        def update_output(level):
+            scatter_data = self.df[["Année universitaire", level, "Nombre d'étudiants", "Dont femmes"]].copy()
+            scatter_data["Année universitaire"] = scatter_data[["Année universitaire"]].applymap(
+                lambda x: x.split('-')[0]
+            )
+            scatter_data["Année universitaire"] = pd.to_datetime(scatter_data["Année universitaire"])
+            scatter_data = scatter_data.groupby(["Année universitaire", level]).sum()
+            scatter_data["Proportion Femmes"] = scatter_data["Dont femmes"] / scatter_data["Nombre d'étudiants"]
+            scatter_data = scatter_data.drop(columns=["Nombre d'étudiants", "Dont femmes"]).unstack(level).fillna(0)
+            scatter_data = scatter_data["Proportion Femmes"]
+
+            fig = px.scatter(scatter_data, trendline="rolling", trendline_options=dict(window=1),
+                             title="Proportion de femmes par regroupement de diplômes",
+                             hover_name=level
+                             )
+            fig.data = [t for t in fig.data if t.mode == "lines"]
+            fig.update_traces(showlegend=True)  # trendlines have showlegend=False by default
+            return fig
+
+        @application.callback(
+            Output("treemap", "figure"),
             Input("year", "value"))
         def display_color(year):
-            fig = px.treemap(cursus_data.loc[self.years[year]], path=["Type d'établissement", "Grande discipline"],
+            fig = px.treemap(self.cursus_data.loc[self.years[year]], path=["Type d'établissement", "Grande discipline"],
                              values="Nombre d'étudiants", color="Proportion Femmes", title=self.years[year],
                              range_color=[0.2, 0.9])
             fig.update_layout(autosize=False,
