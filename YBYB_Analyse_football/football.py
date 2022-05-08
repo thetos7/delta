6013d717c984f0c3a0c0668eace510e37aea3894
BOUARFA DINIA Youssef <youssef.bouarfa-dinia@epita.fr>
2022-05-08 16:07:55 +0200

    done

--- YBYB_Analyse_football/football.py
+++ YBYB_Analyse_football/football.py
@@ -16,14 +16,21 @@ class Football():
         pd.options.plotting.backend = "plotly"
 
         leagues = pd.read_csv("YBYB_Analyse_football/data/concat_leagues/leagues.csv")
+        
+        leagues = leagues.rename(columns = {'Rk': 'Classement', 'Years': 'Saison', 'Age': 'Age Moyen', 'Market Value': 'Valeur Marchande', 'league': 'Ligues'})
+        leagues['Ligues'] = leagues['Ligues'].replace(['liga'],'La Liga')
+        leagues['Ligues'] = leagues['Ligues'].replace(['PL'],'Premier League')
+        leagues['Ligues'] = leagues['Ligues'].replace(['serieA'],'SerieA')
+        leagues['Ligues'] = leagues['Ligues'].replace(['ligue1'],'Ligue 1')
+        leagues['Ligues'] = leagues['Ligues'].replace(['bundesliga'],'Bundesliga')
+        
+        league_colors = {'La Liga':'gold', 'Premier League':'blue', 'SerieA':'green', 'Ligue 1':'black', 'Bundesliga':'red'}
 
-        league_colors = {'liga':'gold', 'PL':'blue', 'serieA':'green', 'ligue1':'black', 'bundesliga':'red'}
-
-        fig = px.scatter(leagues, x='Rk', y='Age', color="league", hover_name="Squad", 
-                color_discrete_map=league_colors, title="Déplacez la souris sur une bulle pour avoir les graphiques du club en bas.",
-                size = 'Market Value', 
-                size_max=60, 
-                animation_frame=leagues["Years"], animation_group="Squad", range_y=[leagues['Age'].min(), leagues['Age'].max()], range_x=[1,20])
+        fig = px.scatter(leagues, x='Classement', y='Age Moyen', color="Ligues", hover_name="Squad", 
+                color_discrete_map=league_colors, title="Déplacez la souris sur une bulle pour avoir les détails du club.",
+                size = 'Valeur Marchande', 
+                size_max=60,
+                animation_frame=leagues["Saison"], animation_group="Squad", range_y=[leagues['Age Moyen'].min(), leagues['Age Moyen'].max()], range_x=[1,20])
                 
         self.main_layout = html.Div(children=[
             html.H3(children='Evolution du classement des équipes en fonction de l âge moyen et de la valeur marchande de leur joueurs'),
@@ -34,13 +41,14 @@ class Football():
             - Nous pouvons donc constater que plus la valeur marchande de l'équipe est élevée plus son classement est meilleur. 
             - Nous constatons aussi que l'equipe a un meilleur classement si la moyenne d'âge de l'équipe est entre 22 ans et 26 ans. 
              En effet une équipe trop jeune manque d'expérience et une équipe agée manque d'intensité dans le jeu. 
-            - Nous remarquons qu'il peut y avoir des exceptions tel que le club Leicester City (en PL) qui est classé premier, dans la saison 2015-16, alors que sa valeur marchande est faible par raport aux autres club.
+            - Nous remarquons qu'il peut y avoir des exceptions tel que le club Leicester City (en Premier League) qui est classé premier, dans la saison 2015-16, alors que sa valeur marchande est faible par raport aux autres club.
             Nous notons aussi que le Bayern Munich (en Bundesliga) est classé premier dans la saison 2005-06 alors que l'âge moyen de ses joueurs est d'environ 27,69 ans. 
             
             ### A propos
             Nous nommons les top 5 ligues de la manière suivante: 
-            - liga -> ligue espagnole
-            - PL -> ligue anglaise 
+            - La Liga -> ligue espagnole
+            - Premier League -> ligue anglaise 
+            - SerieA -> ligue italienne
             - Ligue 1 -> ligue française
             - Bundesliga -> ligue allemande
             * Sources : 
