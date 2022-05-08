28bfab8763217ee36fb0354c9df71049b6f9f496
guillaume.larue <guillaume.larue@epita.fr>
2022-05-08 12:30:20 +0200

    Correct typo and comment the code

--- presidentielle/presidentielle.py
+++ presidentielle/presidentielle.py
@@ -17,18 +17,26 @@ import os
 
 class Presidentielles():
     def __init__(self, application=None):
-        cwd = os.getcwd()
-        print(cwd)
 
+        # ------------------------------------------ #
+        # Création des figures pour les sondages
+        # ------------------------------------------ #
+
+        # Figure tour 1
+        # Lecture du dataset
         df = pd.read_csv('./presidentielle/data/sondages_tour_1.csv')
 
         fig_sond_t1 = px.line(df, y="intentions", x="date_enquete", color="candidat",
-                              title='Sondage en vu du premier tous des présidentielles par candidat')
+                              title='Sondage en vu du premier tous des présidentielles par candidat',
+                              labels={
+                                  "intentions": "Intentions de vote",
+                                  "date_enquete": "Temps",
+                                  "candidat": "Candidats"
+                              })
 
-        self.df_sondage_t1 = df
-
+        # Figure tour 2
+        # Lecture du dataset
         df = pd.read_csv('./presidentielle/data/sondages_tour_2.csv')
-
         df_lepen = df[df["candidat"] == "Marine Le Pen"]
         df_macron = df[df["candidat"] == "Emmanuel Macron"]
 
@@ -55,26 +63,33 @@ class Presidentielles():
                                   xaxis_title='Temps',
                                   yaxis_title='Pourcentage de vote')
 
+        # --------------------------------------------- #
+        # Création des figures pour le temps de parole
+        # --------------------------------------------- #
+
+        # Lecture du dataset
         df_tdp = pd.read_csv('./presidentielle/data/temps_de_parole_presidentielles_2022.csv')
         print(pd.unique(df_tdp['Candidat']))
         print(pd.unique(df_tdp['Période']))
-        # self.df_sondage_t2 = df
-        # self.day_mean = prediction
-        # '2022-04-08'
+
+        # Figure tour 1
         fig_tdp_t1 = px.bar(df_tdp[df_tdp['Période'] == '2022-03-27'], x='Candidat', y="Somme",
                             color='Chaîne', barmode="group", labels={
                 "Période": "Temps en jours",
                 "Somme": "Temps de parole en minutes",
-                "Chaîne": "Chaîne"
-            }, title="Temps de parole pour fache candidats en fonction des médias")
+                "Chaîne": "Chaîne",
+                "Candidat": "Candidats"
+            }, title="Temps de parole pour chaque candidats en fonction des médias")
 
+        # Figure tour 2
         fig_tdp_t2 = px.bar(df_tdp[(df_tdp['Période'] == '2022-04-08') & (
                 (df_tdp['Candidat'] == 'Macron') | (df_tdp['Candidat'] == 'Lepen'))], x='Candidat', y="Somme",
                             color='Chaîne', barmode="group", labels={
                 "Période": "Temps en jours",
                 "Somme": "Temps de parole en minutes",
-                "Chaîne": "Chaîne"
-            }, title="Temps de parole pour fache candidats en fonction des médias")
+                "Chaîne": "Chaîne",
+                "Candidat": "Candidats"
+            }, title="Temps de parole pour chaque candidats en fonction des médias")
 
         self.main_layout = html.Div(children=[
             html.H2(children='Sondages présidentiels et temps de parole dans les médias'),
@@ -95,7 +110,6 @@ class Presidentielles():
             - Certains médias parlent beaucoup plus des élections présidentielles que d'autres
             - Le temps de parole totale des candidats dans les médias ne reflète pas leur passage ou non au second tour 
             (Mme Le Pen a passé beaucoup moins de temps dans les médias que Mme Pecresse et a pourtant accédé au second tour)
-            - 
             """),
             html.H3(children='Second tour'),
             html.Div(
