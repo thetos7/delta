8c5b7f3179b143ecdfe6b1f34188396a0cc9d0a9
Yuishiro <elodine.coquelet@epita.fr>
2022-05-08 18:58:57 +0200

    Final version without cleaning

--- EC_CD_Mariages_et_Divorces_en_France/mariages_divorces.py
+++ EC_CD_Mariages_et_Divorces_en_France/mariages_divorces.py
@@ -12,141 +12,79 @@ import plotly.express as px
 import dateutil as du
 from scipy import stats
 from scipy import fft
-import pandas as pd
-import pandas_datareader as pdr
-import numpy as np
 
 class Mariage():
+    # State for the button
     START = 'Start'
     STOP  = 'Stop'
     
     def __init__(self, application = None):
-        def filter_df(df):
-            df.drop(columns = ['ANAIS1', 'DEPNAIS1', 'INDNAT1', 'ETAMAT1', 'ANAIS2', 'DEPNAIS2', 'INDNAT2','ETAMAT2',
-                    'JSEMAINE', 'DEPDOM', 'TUDOM', 'TUCOM', 'NBENFCOM'], axis=1, inplace=True)
-            
         def convert(i):
             return int(i)
-
-            
+        
+        
+        # List of month name to update the histogram to display month name and not their number
         self.L = ['janv', 'fev', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'sept', 'oct','nov','dec']    
-
-        mar_14 = pd.read_csv("EC_CD_Mariages_et_Divorces_en_France/data/data_mariages_2014.csv", sep=',', low_memory=False)
-        mar_15 = pd.read_csv("EC_CD_Mariages_et_Divorces_en_France/data/data_mariages_2015.csv", sep=',', low_memory=False)
-        mar_16 = pd.read_csv("EC_CD_Mariages_et_Divorces_en_France/data/data_mariages_2016.csv", sep=',', low_memory=False)
-        mar_17 = pd.read_csv("EC_CD_Mariages_et_Divorces_en_France/data/data_mariages_2017.csv", sep=',', low_memory=False)
-        mar_18 = pd.read_csv("EC_CD_Mariages_et_Divorces_en_France/data/data_mariages_2018.csv", sep=',', low_memory=False)
-        mar_19 = pd.read_csv("EC_CD_Mariages_et_Divorces_en_France/data/data_mariages_2019.csv", sep=',', low_memory=False)
-        mar_20 = pd.read_csv("EC_CD_Mariages_et_Divorces_en_France/data/data_mariages_2020.csv", sep=',', low_memory=False)
-        mar = pd.read_excel('EC_CD_Mariages_et_Divorces_en_France/data/mariages par mois.xls', skiprows = {0,1, 2, 3, 5, 6},
-                            usecols = { 'Ensemble des mariages de l\'année', 'Mois du mariage',
-                                       'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre',
-                                       'Octobre', 'Novembre', 'Décembre'}, skipfooter = 1, dtype=int)
-        pacs = pd.read_excel('EC_CD_Mariages_et_Divorces_en_France/data/ensemble des pacs.xlsx',
-                             skiprows = {0,1, 2, 4}, usecols = {0, 4,5,6}, skipfooter = 16)
-        
-        filter_df(mar_14)
-        filter_df(mar_15)
-        filter_df(mar_16)
-        filter_df(mar_17)
-        filter_df(mar_18)
-        filter_df(mar_19)
-        filter_df(mar_20)
-        
-        self.files = [mar_14, mar_15, mar_16, mar_17, mar_18, mar_19, mar_20]
         self.dep = '01'
         
-        deps = pd.read_excel('EC_CD_Mariages_et_Divorces_en_France/data/departements-francais.xls',
-                             usecols = {'NOM', 'NUMÉRO'}, skipfooter = 7)
-        deps.rename(columns={'NUMÉRO' : 'DEPARTEMENT'}, inplace = True)
-            
-        df = pd.concat([mar_14, mar_15, mar_16, mar_17, mar_18, mar_19, mar_20])
-        HH = df[(df['SEXE1'] == 'M') & (df['SEXE2'] == 'M')]
-        graph = pd.DataFrame(HH.groupby('AMAR').size(), columns = ['HH'])
-        FF = df[(df['SEXE1'] == 'F') & (df['SEXE2'] == 'F')]
-        graph = graph.assign(FF = FF.groupby('AMAR').size())
-        HF = df[((df['SEXE1'] == 'M') & (df['SEXE2'] == 'F')) | ((df['SEXE1'] == 'F') & (df['SEXE2'] == 'M'))]
-        graph = graph.assign(HF = HF.groupby('AMAR').size())
-        graph = graph.assign(TOTAL = df.groupby('AMAR').size().astype(int))
+        # Data loading and variable declarations
+        self.table = pd.read_csv('EC_CD_Mariages_et_Divorces_en_France/data/mariage_pour_tous.csv').drop(columns=['Unnamed: 0'])
+        self.histo_1946 = pd.read_csv('EC_CD_Mariages_et_Divorces_en_France/data/mariage_depuis_1946.csv').drop(columns=['Unnamed: 0'])
         
-        map_f = graph.copy().drop(["HH", "FF", "HF", "TOTAL"], axis=1)
-        dep_list = df.groupby("DEPMAR").size().index.tolist()
+        # Copy the data before modification to use them to create the graph (line)
+        self.df = self.histo_1946.copy()
+
+        self.histo_1946 = self.histo_1946.drop(self.histo_1946.iloc[:,1:9], axis=1)
         
-        c = 0
-        for idx in dep_list:
-            col = df[(df['DEPMAR'] == idx)]
-            map_f.insert(c, idx,col.groupby('AMAR').size())
-            c += 1
-        map_f = map_f.transpose()
-        map_f.reset_index(drop = True, inplace = True)
-        map_f = pd.concat([map_f, deps], axis=1)
-        for i in range (0,10):
-            map_f.loc[map_f.DEPARTEMENT == i, "DEPARTEMENT"] = "0%i" % i
-            
-        graph = graph.reset_index()
-        graph['AMAR'] =  graph['AMAR'].apply(convert)    
-            
-            
-            
+        # Selection of the data for the year 1946 (histogram)
+        line = self.histo_1946.iloc[[0]]
         
-        
-        df = mar_14
-        df = df[df['DEPMAR'] == self.dep] 
-      
-        HH = df[(df['SEXE1'] == 'M') & (df['SEXE2'] == 'M')]
-        graph2 = pd.DataFrame(HH.groupby('MMAR').size(), columns = ['HH'])
-        FF = df[(df['SEXE1'] == 'F') & (df['SEXE2'] == 'F')]
-        graph2 = graph2.assign(FF = FF.groupby('MMAR').size())
-        HF = df[((df['SEXE1'] == 'M') & (df['SEXE2'] == 'F')) | ((df['SEXE1'] == 'F') & (df['SEXE2'] == 'M'))]
-        graph2 = graph2.assign(HF = HF.groupby('MMAR').size())
-        
-        graph2 = graph2.reset_index()
-        graph2 = self.check_months(graph2)
-        graph2 = graph2.sort_values(by=['MMAR'])
-        graph2['MMAR'] =  graph2['MMAR'].apply(self.update_month)
-       
         self.year = 1946
-        self.df = graph
-        self.map_f = map_f
+        
+        # Initialisation of the different figures used on the html page
+        self.map_f = pd.read_csv('EC_CD_Mariages_et_Divorces_en_France/data/mariage_par_departement.csv').drop(columns=['Unnamed: 0'])
+                
         self.departements = json.load(open('EC_CD_Mariages_et_Divorces_en_France/data/departements-version-simplifiee.geojson'))
         
-        self.fig = px.line(graph)
+        self.fig = px.line(self.df, x='AMAR', y=['Homme_Homme', 'Femme_Femme', 'Homme_Femme', 'Pacs de personnes de sexe différent', 'Pacs de personnes de même sexe', 'Ensemble des Pacs', 'Total mariage'])
 
         self.fig_map = px.choropleth_mapbox(self.map_f, geojson=self.departements, locations=self.map_f.DEPARTEMENT,
                            hover_name = self.map_f.NOM,
                            featureidkey = 'properties.code', 
-                           color=2014, range_color=[200, 10000], color_continuous_scale="Viridis",
+                           color='2014', range_color=[200, 10000], color_continuous_scale="Viridis",
                            mapbox_style="carto-positron",
                            zoom=4.6, center = {"lat": 47, "lon": 2},
                            opacity=0,
                            labels={self.year:'Nombre de Mariages'}
                           )
         self.fig_map.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-        self.fig_histo = px.histogram(graph2, x = 'MMAR', y = ['HH', 'FF', 'HF'], barmode='group')
+        self.fig_histo = px.histogram(line, x='AMAR', y=['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août',
+                                                         'Septembre', 'Octobre', 'Novembre', 'Décembre'], barmode='group')
         self.fig.update_layout(
-            title = 'Mariages en France depuis 2014',
+            title = 'Mariages et Pacs en France depuis 1946',
             xaxis = dict(title = 'Année de l\'Union'),
             yaxis = dict(title = 'Nombre d\'Union'), 
             legend = dict(title = 'Type d\'Union')
         )
         self.fig_histo.update_layout(
-            title = 'Mariage et divorce %s en %s' % (self.dep_name(), self.year),
+            title = 'Mariages en %s' % (self.year),
             xaxis = dict(title = 'Mois du mariage'),
-            yaxis = dict(title = 'Nombre de mariages'), 
-            legend = dict(title = 'Type de mariage') 
+            yaxis = dict(title = ''), 
+            legend = dict(title = 'Nombre de mariages') 
         )
-               
+         
+        # Initialisation of the html page
         self.main_layout = html.Div(children=[
             html.H3(children='Mariages en France'),
             html.Div([
                 dcc.Graph(
                     id='mdf-map-graph',
-                    #figure=self.fig_map,
-                    style={'width':'50%', }),
+                    figure=self.fig_map,
+                    style={'width':'40%', }),
                 dcc.Graph(
                     id='mdf-main-graph',
                     figure=self.fig,
-                    style={'width':'50%', 'display':'inline-block'}),
+                    style={'width':'60%', 'display':'inline-block'}),
                 ], style={ 'display':'flex', 
                        'borderTop': 'thin lightgrey solid',
                        'borderBottom': 'thin lightgrey solid',
@@ -181,12 +119,14 @@ class Mariage():
                         html.Br(),
             dcc.Markdown(
                 """
-                La carte est interactive. En cliquant sur un département, l'histogramme affichera les données sur le département sélectionné. 
+                La carte devient interactive à partir de 2014. En cliquant sur un département, l'histogramme affichera les données sur le département sélectionné. 
                 Notes :
-               * 17 mai 2013
-               * HH : Mariage Homme-Homme
-               * FF : Mariage Femme-Femme
-               * HF : Mariage Homme-Femme
+                       * 1945 : Fin de la IIe guerre mondiale (explosion de mariages)
+                       * 15 novembre 1999 : Création des PACS
+                       * 17 mai 2013 : Mariage pour tous
+                       * HH : Mariage Homme-Homme
+                       * FF : Mariage Femme-Femme
+                       * HF : Mariage Homme-Femme
             #### À propos
             * Sources : https://www.insee.fr/fr/statistiques
             * (c) 2022 Elodine Coquelet & Calliopee Desenfans
@@ -203,36 +143,37 @@ class Mariage():
         else:
             self.app = dash.Dash(__name__)
             self.app.layout = self.main_layout
-
+        
+        # Callback to update the map and histogram
         self.app.callback(
             dash.dependencies.Output('mdf-map-graph', 'figure'),
             dash.dependencies.Output('mdf-histo-graph', 'figure'),
             dash.dependencies.Input('mdf-crossfilter-year-slider', 'value'),
             dash.dependencies.Input('mdf-map-graph', 'clickData'))(self.update_graph)
+        # Change the status of the button (START or STOP)
         self.app.callback(
             dash.dependencies.Output('mdf-button-start-stop', 'children'),
             dash.dependencies.Input('mdf-button-start-stop', 'n_clicks'),
             dash.dependencies.State('mdf-button-start-stop', 'children'))(self.change_button)
+        # Update automatically the slider when the animation is not stop
         self.app.callback(
             dash.dependencies.Output('mdf-auto-stepper', 'max_interval'),
             [dash.dependencies.Input('mdf-button-start-stop', 'children')])(self.run_movie)
+        # Update the variables depending on the value of the slider. Triggered by the previous callback
         self.app.callback(
             dash.dependencies.Output('mdf-crossfilter-year-slider', 'value'),
             dash.dependencies.Input('mdf-auto-stepper', 'n_intervals'),
             [dash.dependencies.State('mdf-crossfilter-year-slider', 'value'),
             dash.dependencies.State('mdf-button-start-stop', 'children')])(self.on_interval)
 
-
-    def get_file(self):
-           return self.files[self.year - 2014].sort_values(by=['MMAR'])
-        
+    # Update the map and make it visible in 2014 (opacity=opa)    
     def update_map(self):
         opa = 0 if self.year < 2014 else 0.5
         year = self.year if self.year > 2013 else 2014
         self.fig_map = px.choropleth_mapbox(self.map_f, geojson=self.departements, locations= self.map_f.DEPARTEMENT,
                            hover_name = self.map_f.NOM,
                            featureidkey = 'properties.code', 
-                           color=year, range_color=[200, 10000], color_continuous_scale="Viridis",
+                           color=str(year), range_color=[200, 10000], color_continuous_scale="Viridis",
                            mapbox_style="carto-positron",
                            zoom=4.6, center = {"lat": 47, "lon": 2},
                            opacity=opa,
@@ -240,12 +181,23 @@ class Mariage():
                           )
         self.fig_map.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     
-    
-    #TODO : mettre le nom département en titre Saloperie de Corse option de secours faire une fonction qui retourne ce qu'il faut
+    # Update the histogram depending of the year
     def update_histo(self):
+        # Display a histogram of the number of mariage by month during the year
         if self.year < 2014:
+            line = self.histo_1946.iloc[[self.year - 1946]]
+            self.fig_histo = px.histogram(line, x='AMAR', y=['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août',
+                                                             'Septembre', 'Octobre', 'Novembre', 'Décembre'], barmode='group')
+            self.fig_histo.update_layout(
+                title = 'Mariages en %s' % (self.year),
+                xaxis = dict(title = ''),
+                yaxis = dict(title = 'Nombre de mariages'), 
+                legend = dict(title = 'Mois de mariage') 
+            )
             return
-        df = self.get_file()
+        
+        # Display the number of mariages and their type (HH, FF ou HF) by month for a department during the year
+        df = self.table[self.table['AMAR'] == self.year]
         df = df[df['DEPMAR'] == self.dep]
       
         HH = df[(df['SEXE1'] == 'M') & (df['SEXE2'] == 'M')]
@@ -262,23 +214,25 @@ class Mariage():
         
         self.fig_histo = px.histogram(graph, x = 'MMAR', y = ['HH', 'FF', 'HF'], barmode='group')
         
-        self.dep_name
         self.fig_histo.update_layout(
             title = 'Mariage et divorce %s en %s' % (self.dep_name(), self.year),
             xaxis = dict(title = 'Mois du mariage'),
             yaxis = dict(title = 'Nombre de mariages'), 
             legend = dict(title = 'Type de mariage') 
         )
-        
+    
+    # Trigger the update of the map and the histogram
     def update_graph(self, year, clickData):
         ctx = dash.callback_context
         button_id =""
         if ctx.triggered:
             button_id = ctx.triggered[0]['prop_id'].split('.')[0]
             
+        # Check if the user moved the slider  
         if button_id == 'mdf-crossfilter-year-slider':
             self.year = year
             self.update_map()
+        # Check if the user click on the map to select a department
         if button_id == 'mdf-map-graph':
             self.dep = str(clickData['points'][0]['location'])
         self.update_histo()
@@ -290,9 +244,11 @@ class Mariage():
             return 'MF'
         return s
         
+    # Replace the months numbers by their name
     def update_month(self, s):
         return self.L[int(s) - 1]
     
+    # Add to the data the month without any mariages
     def check_months(self, graph):
         col = graph['MMAR'].unique()
         idx = len(col)
@@ -303,28 +259,30 @@ class Mariage():
             else:
                 s = str(i)
             if i not in col :
+                # The value '0.0001' allows the month to appear on the histogram
                 graph.loc[idx] = (int(s), 0,0, 0.0001)
                 idx += 1
         return graph
     
+    # Return the name of the department based on its number
     def dep_name(self):
-        if self.dep == "2A" or self.dep == "2B" or int(self.dep) < 10:
             return self.map_f[self.map_f['DEPARTEMENT'] == self.dep].iloc[0,7]
-        else:
-            return self.map_f[self.map_f['DEPARTEMENT'] == int(self.dep)].iloc[0,7]
     
+    # Update the state of the button
     def change_button(self, n_clicks, text):
         if text == self.START:
             return self.STOP
         else:
             return self.START
     
+    # Launch and stop the animation
     def run_movie(self, text):
         if text == self.START:
             return 0 
         else:
             return -1
-        
+    
+    # Update the variable to create the animation
     def on_interval(self, n_intervals, year, text):
         if text == self.STOP:
             if self.year < 2014:
