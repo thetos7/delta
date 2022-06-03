b73093bab09d5c23f2ef06674a0f9513c75a934f
Julien Cros <julien.cros@epita.fr>
2022-06-03 21:08:09 +0200

    final completement final

--- delta.py
+++ delta.py
@@ -61,6 +61,7 @@ from formations import formations as formations_lib
 from APAAL_criminalite_education import criminalite_education
 from ADHD_Movies import movies
 from ab_wg_apb_parcoursup import apb_parcoursup
+from corporate_impact import corp_impact
 
 #@profile
 def init():
@@ -121,6 +122,8 @@ def init():
     crim_edu = criminalite_education.Criminalite_Education(app)
     mvs = movies.MoviesStats(app)
     apb = apb_parcoursup.APB_PARCOURSUP(app)
+    c_i = corp_impact.CorporateImpact(app)
+    
 
     # external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -188,6 +191,7 @@ def init():
                                   dcc.Link(html.Button("Criminalité et Education", style={"width": "100%"}), href="/criminalite-education"),
                                   dcc.Link(html.Button('Rentabilité des films', style={'width':"100%"}), href='/ADHD_Movies'),
                                   dcc.Link( html.Button("APB / Parcoursup", style={"width": "100%"}), href="/ab-wg_apb-parcoursup",),
+                                  dcc.Link(html.Button('Corporate Envt Impact', style={'width':"100%"}), href='/corp_impact'),
                                   html.Br(),
                                   html.Br(),
                                   html.Br(),
@@ -335,6 +339,8 @@ def init():
             return mvs.main_layout
         elif pathname == "/ab-wg_apb-parcoursup":
             return apb.main_layout
+        elif pathname == '/corp_impact':
+            return c_i.main_layout
         else:
             return home_page
     return app
